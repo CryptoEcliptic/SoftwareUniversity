6379765596afba711c1b0be56d6e47e472a8eaba
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-03-23 16:10:32 +0200

    Drawing figures with loops

---
+++ Programing Basics C#/Exam Tasks/05CrownFigure/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05CrownFigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("@{0}@{0}@", new string(' ', n - 2));
+            Console.WriteLine("**{0}*{0}**", new string(' ', n - 3));
+
+            for (int i = 0; i < n / 2 - 2; i++)
+            {
+                // for {1} is important to find non negative value. Cant say dots-- or dots -=2;;
+                Console.WriteLine("*{0}*{1}*{2}*{1}*{0}*", new string('.', i + 1), new string(' ', n - 5 - 2 * i), new string('.', 1 + 2 * i));
+            }
+            Console.WriteLine("*{0}*{1}*{0}*", new string('.', n / 2 - 1), new string('.', 1 + 2 * (n / 2 - 2)));
+
+            Console.WriteLine("*{0}{1}.{1}{0}*", new string('.', n / 2), new string('*', n / 2 - 2));
+
+            Console.WriteLine(new string('*', 2 * n - 1));
+            Console.WriteLine(new string('*', 2 * n - 1));
+        }
+    }
+}
---
+++ Programing Basics C#/Exam Tasks/05ParallelepipedFigure/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05ParallelepipedFigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int wavesNumber = n - 2;
+            int startandEndDots = 2 * n + 1;
+            int rightDots = 2 * n;
+
+            Console.WriteLine("+{0}+{1}", new string('~', wavesNumber), new string('.', startandEndDots));
+            for (int i = 0; i <= 2 * n; i++)
+            {
+                int leftDots = i;
+                Console.WriteLine("|{0}\\{1}\\{2}", new string('.', leftDots), new string('~', wavesNumber), new string('.', rightDots));
+                rightDots--;
+            }
+
+            int lowerRightDots = 2 * n;
+            for (int i = 0; i <= 2 * n; i++)
+            {
+                int lowerleftDots = i;
+                Console.WriteLine("{0}\\{1}|{2}|", new string('.', lowerleftDots), new string('.', lowerRightDots), new string('~', wavesNumber));
+                
+                lowerRightDots--;
+            }
+            Console.WriteLine("{0}+{1}+", new string('.', startandEndDots), new string('~', wavesNumber));
+        }
+    }
+}
