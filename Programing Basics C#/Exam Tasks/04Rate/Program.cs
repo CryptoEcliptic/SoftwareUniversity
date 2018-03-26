7eaeb70f02c97e4646424eb5e0041cab012f0fcc
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-03-26 15:17:55 +0300

    Softuni Exam Task

---
+++ Programing Basics C#/Exam Tasks/04Rate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04Rate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double startAmount = double.Parse(Console.ReadLine());
+            int numberMonths = int.Parse(Console.ReadLine());
+            double simpleSum = startAmount;
+            double complexSum = startAmount;
+
+            for (int i = 1; i <= numberMonths;  i++)
+            {
+                simpleSum = simpleSum + (startAmount * 0.03);
+                complexSum = complexSum + (complexSum * 0.027);
+            }
+            Console.WriteLine($"Simple interest rate: {simpleSum:f2} lv.");
+            Console.WriteLine($"Complex interest rate: {complexSum:f2} lv.");
+            double difference = Math.Abs(simpleSum - complexSum);
+            if (simpleSum > complexSum)
+            {
+                Console.WriteLine($"Choose a simple interest rate. You will win {difference:f2} lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Choose a complex interest rate. You will win {difference:f2} lv.");
+            }
+        }
+    }
+}
