e1350b7acf01f6910a7f4b5927dcb243d27ccff8
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-01 14:29:13 +0300

    SoftUni Exam Task

---
+++ Programing Basics C#/Exam Tasks/04Balls/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04Balls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ballsCount = int.Parse(Console.ReadLine());
+            int totalPoints = 0;
+            int redBalls = 0;
+            int yellowBalls = 0;
+            int orangeBalls = 0;
+            int whiteBalls = 0;
+            int blackBalls = 0;
+            int otherColours = 0;
+            for (int i = 1; i <= ballsCount; i++)
+            {
+                string ballColour = Console.ReadLine();
+
+                if (ballColour == "red")
+                {
+                    totalPoints += 5;
+                    redBalls++;
+                }
+                else if (ballColour == "orange")
+                {
+                    totalPoints += 10;
+                    orangeBalls++;
+                }
+                else if (ballColour == "yellow")
+                {
+                    totalPoints += 15;
+                    yellowBalls++;
+                }
+                else if (ballColour == "white")
+                {
+                    totalPoints += 20;
+                    whiteBalls++;
+                }
+                else if (ballColour == "black")
+                {
+                    totalPoints = totalPoints / 2;
+                    blackBalls++;
+                }
+                else
+                {
+                    otherColours++;
+                }
+            }
+            Console.WriteLine($"Total points: {totalPoints}");
+            Console.WriteLine($"Points from red balls: {redBalls}\nPoints from orange balls: {orangeBalls}");
+            Console.WriteLine($"Points from yellow balls: {yellowBalls}\nPoints from white balls: {whiteBalls}");
+            Console.WriteLine($"Other colors picked: {otherColours}\nDivides from black balls: {blackBalls}");
+        }
+    }
+}
---
+++ Programing Basics C#/Exam Tasks/06NumberGenerator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06NumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+            int specialNum = int.Parse(Console.ReadLine());
+            int controlNum = int.Parse(Console.ReadLine());
+
+
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = m; j >= 1; j--)
+                {
+
+                    for (int k = l; k >= 1; k--)
+                    {
+                        int sum = i * 100 + j * 10 + k;
+
+                        if (sum % 3 == 0)
+                        {
+                            specialNum += 5;
+                        }
+                        else if (sum % 10 == 5)
+                        {
+                            specialNum -= 2;
+                        }
+                        else if (sum % 2 == 0)
+                        {
+                            specialNum = specialNum * 2;
+
+                        }
+                        if (specialNum >= controlNum)
+                        {
+                            Console.WriteLine("Yes! Control number was reached! Current special number is {0}.", specialNum);
+                            return;
+                        }
+                    }
+                }
+
+            }
+            Console.WriteLine("No! {0} is the last reached special number.", specialNum);
+        }
+    }
+}
