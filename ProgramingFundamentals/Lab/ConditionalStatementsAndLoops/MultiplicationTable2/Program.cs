0dc1b6491bea3f4ec2105e16c43e9edb1ff25480
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-02 13:50:02 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/ConditionalStatementsAndLoops/MultiplicationTable2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double times = double.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine($"{number} X {times} = {number * times}");
+                times++;
+            } while (times <= 10);
+        }
+    }
+}
