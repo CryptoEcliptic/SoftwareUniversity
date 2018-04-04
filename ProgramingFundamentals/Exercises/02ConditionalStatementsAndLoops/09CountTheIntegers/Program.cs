294b0030b3d8c8f4f7768f8fd8f2eb8ff02acab7
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-04 16:57:31 +0300

    while loop with try/catch check

---
+++ ProgramingFundamentals/Exercises/02ConditionalStatementsAndLoops/09CountTheIntegers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09CountTheIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            while (true)
+            {
+                try
+                {
+                    int n = int.Parse(Console.ReadLine());
+                    count++;
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
