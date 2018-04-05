012b8b7489efa25b165b49f5305bc0c17e846007
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-05 17:45:00 +0300

    Data types and difference between variables

---
+++ ProgramingFundamentals/Exercises/03DataTypesAndVariables/03PracticeCharactersAndStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03PracticeCharactersAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Software University";
+            char capital = 'B';
+            char small1 = 'y';
+            char small2 = 'e';
+            string sentance = "I love programming";
+            Console.WriteLine($"{name}\r\n{capital}\r\n{small1}\r\n{small2}\r\n{sentance}");
+        }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/03DataTypesAndVariables/04VariableInHexFromat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04VariableInHexFromat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hexOne = Convert.ToInt32(Console.ReadLine(), 16);
+            Console.WriteLine(hexOne);
+        }
+    }
+}
