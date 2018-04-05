9b78717efb902e8a67a06a995f044e7ee31aae1c
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-05 17:28:51 +0300

    Homework exercises

---
+++ ProgramingFundamentals/Exercises/02ConditionalStatementsAndLoops/14MagicLetter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14MagicLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+            char symbolOmitted = char.Parse(Console.ReadLine());
+
+            for (char i = start; i <= end; i++)
+            {
+                for (char j = start; j <= end; j++)
+                {
+                    for (char k = start; k <= end; k++)
+                    {
+                        if (i != symbolOmitted && j != symbolOmitted && k != symbolOmitted)
+                        {
+                            Console.Write($"{i}{j}{k} ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
