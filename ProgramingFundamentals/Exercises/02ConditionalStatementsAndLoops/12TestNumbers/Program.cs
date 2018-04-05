08306d343ea09080ad1d47cb23dc5553dbde4d73
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-05 17:26:09 +0300

    Homework exercises

---
+++ ProgramingFundamentals/Exercises/02ConditionalStatementsAndLoops/12TestNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12TestNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int maxSumBoundary = int.Parse(Console.ReadLine());
+            int totalSum = 0;
+            int count = 0;
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    totalSum += (i * j) * 3;
+                    count++;
+                    if (totalSum >= maxSumBoundary)
+                    {
+                        Console.WriteLine($"{count} combinations\r\nSum: {totalSum} >= {maxSumBoundary}");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{count} combinations\r\nSum: {totalSum}");
+        }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/02ConditionalStatementsAndLoops/13GameOfNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13GameOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int count = 0;
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                for (int j = startNumber; j <= endNumber; j++)
+                {
+                    sum = i + j;
+                    count++;
+                    if (sum == magicNumber)
+                    {
+                        Console.WriteLine($"Number found! {j} + {i} = {sum}");
+                        return;
+                    }
+
+                }
+            }
+            Console.WriteLine($"{count} combinations - neither equals {magicNumber}");
+        }
+    }
+}
