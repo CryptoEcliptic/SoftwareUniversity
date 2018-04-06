8a8f1630f917fe4652962a91747c2a590221d427
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-06 16:19:10 +0300

    Homework exercises

---
+++ ProgramingFundamentals/Exercises/03DataTypesAndVariables/10CenturiesToNanoseconds/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10CenturiesToNanoseconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte centuries = byte.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days = (int)(years * 365.2422);
+            int hours = days * 24;
+            long minutes = hours * 60;
+            long seconds = minutes * 60;
+            long milliseconds = seconds * 1000;
+            ulong microseconds = (ulong) milliseconds * 1000;
+            decimal nanoseconds = microseconds * 1000.00m;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes " +
+                $"= {seconds} seconds = {milliseconds} milliseconds = {microseconds} microseconds = {nanoseconds:f0} nanoseconds");
+
+        }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/03DataTypesAndVariables/11ConvertSpeedUnits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11ConvertSpeedUnits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float distanceMeters = float.Parse(Console.ReadLine());
+            float hours = float.Parse(Console.ReadLine());
+            float minutes = float.Parse(Console.ReadLine());
+            float seconds = float.Parse(Console.ReadLine());
+
+            float totalTimeSeconds = (hours * 3600) + (minutes * 60) + seconds; //What is the total time in seconds. Used for calculation spped m/s.
+            float totalHours = hours + (minutes / 60) + (seconds / 3600); // real number for total time in hours. Used for speed Per Hour.
+            float speedMetersPerSecond = distanceMeters / totalTimeSeconds;
+            float speedKilometersPerHour = (distanceMeters / 1000) / totalHours;
+            float speedMilesPerHour = (distanceMeters / 1609) / totalHours;
+
+            Console.WriteLine(speedMetersPerSecond);
+            Console.WriteLine(speedKilometersPerHour);
+            Console.WriteLine(speedMilesPerHour);
+        }
+    }
+}
