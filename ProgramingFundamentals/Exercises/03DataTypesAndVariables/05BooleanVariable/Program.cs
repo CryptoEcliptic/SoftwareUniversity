0875d0013b861664f50e77d6b938d656e50510e6
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-06 13:26:43 +0300

    Converting to boolean

---
+++ ProgramingFundamentals/Exercises/03DataTypesAndVariables/05BooleanVariable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05BooleanVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool inputToBoolean = Convert.ToBoolean(Console.ReadLine());
+            if (inputToBoolean == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (inputToBoolean == false)
+            {
+                Console.WriteLine("No");
+            }
+
+        }
+    }
+}
