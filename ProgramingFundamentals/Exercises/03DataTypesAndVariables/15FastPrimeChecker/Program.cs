580d08195880b59fa22ed3d770c0b562664f5329
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-09 16:51:14 +0300

    Homework exercises

---
+++ ProgramingFundamentals/Exercises/03DataTypesAndVariables/15FastPrimeChecker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15FastPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberLimit = int.Parse(Console.ReadLine());
+            for (int number = 2; number <= numberLimit; number++)
+            {
+                bool isNumberPrime = true;
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if (number % i == 0)
+                    {
+                        isNumberPrime = false;
+                    }
+                }
+                Console.WriteLine($"{number} -> {isNumberPrime}");
+            }
+        }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/03DataTypesAndVariables/16ComparingFloats/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16ComparingFloats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberA = double.Parse(Console.ReadLine());
+            double numberB = double.Parse(Console.ReadLine());
+            bool isAandBequal = false;
+            if (Math.Abs(numberA - numberB) < 0.000001)
+            {
+                isAandBequal = true;
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
