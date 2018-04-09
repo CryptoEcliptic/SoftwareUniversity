ea60eb74fd93b79a901c39c5882937ca8c4f0434
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-09 16:47:43 +0300

    if (char.IsNumber(symbol))

---
+++ ProgramingFundamentals/Exercises/03DataTypesAndVariables/13VowelOrDigit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13VowelOrDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = char.Parse(Console.ReadLine());
+
+            if (symbol == 'a' || symbol == 'o' || symbol == 'e' || symbol == 'i' || symbol == 'u' || symbol == 'y')
+            {
+                Console.WriteLine("vowel");
+            }
+            else if (char.IsNumber(symbol))
+            {
+                Console.WriteLine("digit");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}
