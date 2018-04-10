b5e441b58b4a66a71d6471177e91c4235fe710c8
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-10 19:42:16 +0300

    Homework exercises

---
+++ ProgramingFundamentals/Exercises/03DataTypesAndVariablesMoreExercises/01TypeBoundaries/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("01TypeBoundaries")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("01TypeBoundaries")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f340feb8-08f5-45ea-b971-4dbb4422b2b2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProgramingFundamentals/Exercises/03DataTypesAndVariablesMoreExercises/02NumberChecker/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02NumberChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberString = Console.ReadLine();
+            bool integer = false;
+            bool floating = false;
+            try
+            {
+                long intNum = long.Parse(numberString);
+                integer = true;
+            }
+            catch (Exception)
+            {
+            }
+            try
+            {
+                double doubleNum = double.Parse(numberString);
+                floating = true;
+            }
+            catch (Exception)
+            {
+            }
+            if (integer == true)
+            {
+                Console.WriteLine("integer");
+            }
+            else if (floating == true)
+            {
+                Console.WriteLine("floating-point");
+            }
+        }
+    }
+}
