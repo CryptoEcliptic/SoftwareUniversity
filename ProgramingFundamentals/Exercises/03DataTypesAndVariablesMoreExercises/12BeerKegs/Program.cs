f6fd8663cbc94d2bb2ed5e444c432d214c9da600
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-11 13:35:38 +0300

    Homework exercises

---
+++ ProgramingFundamentals/Exercises/03DataTypesAndVariablesMoreExercises/12BeerKegs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberOfKegs = int.Parse(Console.ReadLine());
+            double biggestKegVolume = 0; // Temporary variable to accept the value of kegVolume
+            double PI = Math.PI;
+            string kegModel = ""; // Temporary variable to accept the value of currentKegModel
+            for (int i = 1; i <= numberOfKegs; i++)
+            {
+                string currnetKegModel = Console.ReadLine();
+                double kegRadius = double.Parse(Console.ReadLine());
+                double kegHeight = double.Parse(Console.ReadLine());
+                double kegVolume = PI * kegRadius * kegRadius * kegHeight;
+                if (kegVolume > biggestKegVolume)
+                {
+                    biggestKegVolume = kegVolume;
+                    kegModel = currnetKegModel;
+                }
+            }
+            Console.WriteLine(kegModel);
+        }
+    }
+}
