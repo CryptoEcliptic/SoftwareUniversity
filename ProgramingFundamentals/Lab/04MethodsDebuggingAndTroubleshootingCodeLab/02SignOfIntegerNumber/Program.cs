f59ccdecc2c640c2f8f1e22d3d79b2c5289379ce
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-12 17:04:23 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/04MethodsDebuggingAndTroubleshootingCodeLab/02SignOfIntegerNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02SignOfIntegerNumber
+{
+    class Program
+    {
+        static void PrintNumber(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintNumber(n);
+        }
+    }
+}
