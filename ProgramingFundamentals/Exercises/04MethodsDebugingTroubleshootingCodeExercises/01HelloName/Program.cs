a59c9d7dda38db5dd412428ba974ae9b2c7048cb
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-13 19:19:10 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/04MethodsDebugingTroubleshootingCodeExercises/01HelloName/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01HelloName
+{
+    class Program
+    {
+        static string NameGreeting(string name)
+        {
+            return ($"Hello, {name}!");
+        }
+
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine(NameGreeting(name));
+        }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/04MethodsDebugingTroubleshootingCodeExercises/02MaxMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02MaxMethod
+{
+    class Program
+    {
+        static int GetMax(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int firtsInput = int.Parse(Console.ReadLine());
+            int secondInput = int.Parse(Console.ReadLine());
+            int thirdInput = int.Parse(Console.ReadLine());
+            int maximalInput = GetMax(firtsInput, secondInput);
+            maximalInput = GetMax(maximalInput, thirdInput);
+
+            Console.WriteLine(maximalInput);
+        }
+    }
+}
