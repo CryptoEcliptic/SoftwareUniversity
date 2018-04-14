1993fdbff089604af84c105ae1d0f814d61ad401
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-14 18:12:43 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/04MethodsDebugingTroubleshootingCodeExercises/03EnglishNameLastDigit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03EnglishNameLastDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long number = long.Parse(Console.ReadLine());
+            long lastNum = LastDigit(number);
+            string[] tonine = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+            Console.WriteLine(tonine[lastNum]);
+        }
+        static long LastDigit(long n)
+        {
+            long lastNum = Math.Abs(n % 10);
+            return lastNum;
+        }
+    }
+}
