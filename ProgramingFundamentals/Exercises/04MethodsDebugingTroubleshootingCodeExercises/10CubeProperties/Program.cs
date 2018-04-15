71829144124c328ff46b64fb04bb7b1a111d1c6f
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-15 16:58:00 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/04MethodsDebugingTroubleshootingCodeExercises/10CubeProperties/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10CubeProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double side = double.Parse(Console.ReadLine());
+            string parameter = Console.ReadLine().ToLower();
+
+            double volumeCube = Math.Pow(side, 3);
+            double faceDiagonalsCube = Math.Sqrt(2 * Math.Pow(side, 2));
+            double spaceDiagonalsCube = Math.Sqrt(3 * Math.Pow(side, 2));
+            double surfaceAreaCube = 6 * Math.Pow(side, 2);
+            if (parameter == "face")
+            {
+                Console.WriteLine($"{faceDiagonalsCube:f2}");
+            }
+            else if (parameter == "space")
+            {
+                Console.WriteLine($"{spaceDiagonalsCube:f2}");
+            }
+            else if (parameter == "volume")
+            {
+                Console.WriteLine($"{volumeCube:f2}");
+            }
+            else if (parameter == "area")
+            {
+                Console.WriteLine($"{surfaceAreaCube:f2}");
+            }
+        }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/04MethodsDebugingTroubleshootingCodeExercises/11GeometryCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11GeometryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeFigure = Console.ReadLine().ToLower();
+
+            if (typeFigure == "triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                double triangleArea = (height * side) / 2;
+                Console.WriteLine($"{triangleArea:f2}");
+            }
+            else if (typeFigure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double squareArea = side * side;
+                Console.WriteLine($"{squareArea:f2}");
+            }
+            else if (typeFigure == "rectangle")
+            {
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                double rectangleArea = width * height;
+                Console.WriteLine($"{rectangleArea:f2}");
+            }
+            else if (typeFigure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                double circleArea = Math.PI * (radius * radius);
+                Console.WriteLine($"{circleArea:f2}");
+            }
+
+        }
+    }
+}
