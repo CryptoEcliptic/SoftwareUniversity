3084a4e97019a0639b9946a62e31fa2931469133
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-16 16:41:05 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/04MethodsDebugingTroubleshootingCodeExercises/13Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace _13Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger inputNumber = BigInteger.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+            for (int i = 1; i <= inputNumber; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine(factorial); //Example for factorial 1 * 2 * 3 * 4 * 5 = 120
+        }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/04MethodsDebugingTroubleshootingCodeExercises/14FactorialTrailing Zeroes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace _14FactorialTrailing_Zeroes
+{
+    class Program
+    {
+        private static BigInteger CalculatingFactorial(BigInteger number)
+        {
+            BigInteger factorial = number;
+            for (int i = 1; i < number; i++)
+            {
+                factorial *= i;
+
+            }
+            return factorial;
+        }
+        static BigInteger TailingZeroes(BigInteger num)
+        {
+            BigInteger tailingZeroes = 0;
+            while (num % 10 == 0)
+            {
+                num = num / 10;
+                tailingZeroes++;
+            }
+            return tailingZeroes;
+        }
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            BigInteger factorial = CalculatingFactorial(input);
+            BigInteger tailingZeros = TailingZeroes(factorial);
+            Console.WriteLine(tailingZeros);
+        }
+    }
+}
