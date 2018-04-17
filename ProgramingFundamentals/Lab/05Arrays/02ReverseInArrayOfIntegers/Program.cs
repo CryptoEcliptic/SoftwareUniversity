0cc440bf70c56257804f819f5903d5e875cdc1e3
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-17 17:12:41 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/05Arrays/02ReverseInArrayOfIntegers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02ReverseInArrayOfIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberElements = int.Parse(Console.ReadLine());
+            int[] numbers = new int[numberElements];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            int[] reversedNumbers = numbers.Reverse().ToArray();
+            foreach (var reversedNumber in reversedNumbers)
+            {
+                Console.Write(reversedNumber + " ");
+            }
+        }
+    }
+}
