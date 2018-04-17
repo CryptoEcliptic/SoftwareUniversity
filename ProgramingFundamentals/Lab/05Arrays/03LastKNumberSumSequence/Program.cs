d991033a78a61d261bea0242a39ef1ff6c4416f5
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-17 17:14:05 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/05Arrays/03LastKNumberSumSequence/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03LastKNumberSumSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numElements = int.Parse(Console.ReadLine());
+            int kStep = int.Parse(Console.ReadLine());
+
+            long[] numbers = new long[numElements];
+            numbers[0] = 1;
+            
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                long sum = 0;
+                for (int j = i - kStep; j <= i - 1; j++)
+                {
+                    if (j >= 0)
+                    {
+                        sum += numbers[j];
+                    }
+                    numbers[i] = sum;
+                }
+            }
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ProgramingFundamentals/Lab/05Arrays/04RoundingNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05RoundingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputNumbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            int[] roundNUmbers = new int[inputNumbers.Length] ;
+
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+               
+              roundNUmbers[i] = (int)Math.Round(inputNumbers[i], MidpointRounding.AwayFromZero);
+
+              
+            }
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                Console.WriteLine($"{inputNumbers[i]} => {roundNUmbers[i]}");
+            }
+
+        }
+    }
+}
