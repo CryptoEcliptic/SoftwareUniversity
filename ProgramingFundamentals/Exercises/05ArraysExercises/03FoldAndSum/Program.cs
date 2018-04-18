c667cfe0c9273ae4c428783cefd2a64b1f36a8b2
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-18 19:02:06 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/05ArraysExercises/03FoldAndSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int k = input.Length / 4;
+            int[] leftPart = input.Take(k).ToArray();
+            int[] middlePart = input.Skip(k).Take(2 * k).ToArray();
+            int[] rightPart = input.Skip(3 * k).Take(k).ToArray();
+
+            leftPart = leftPart.Reverse().ToArray();
+            rightPart = rightPart.Reverse().ToArray();
+            int[] result = new int[2 * k];
+            for (int i = 0; i < k; i++)
+            {
+                result[i] = leftPart[i] + middlePart[i];
+                result[i + k] = middlePart[i + k] + rightPart[i];   
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/05ArraysExercises/04SieveOfEratosthenes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04SieveOfEratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            bool[] primeNumbers = new bool[inputNumber + 1];
+
+            for (int i = 2; i < primeNumbers.Length; i++)
+            {
+                primeNumbers[i] = true;
+            }
+            for (int i = 2; i < Math.Sqrt(inputNumber); i++)
+            {
+                if (primeNumbers[i] == true)
+                {
+                    for (int j = i * i; j <= inputNumber; j += i)
+                    {
+                        primeNumbers[j] = false;
+                    }
+                }
+            }
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < primeNumbers.Length; i++)
+            {
+                if (primeNumbers[i] == true)
+                {
+                    result.Add(i);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
