96171260c8f161d3f1fc49525a6576d4c5d2176a
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-18 18:48:41 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/05Arrays/08CondenseArrayToNymber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08CondenseArrayToNymber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+         
+            while (nums.Length > 1)
+            {
+                int[] condensedNumbers = new int[nums.Length - 1];
+
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    condensedNumbers[i] = nums[i] + nums[i + 1];
+                }
+                nums = condensedNumbers;
+            }
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
