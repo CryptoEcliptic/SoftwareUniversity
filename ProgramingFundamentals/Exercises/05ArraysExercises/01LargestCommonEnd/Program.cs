3ded6fa7503fe487b58eeb5d5b19adacbc688af3
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-18 19:00:17 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/05ArraysExercises/01LargestCommonEnd/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string[] array2 = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            int length = Math.Min(array1.Length, array2.Length);
+            int counter = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (array1[i] == array2[i])
+                {
+                    counter++;
+                }
+            }
+
+            array1 = array1.Reverse().ToArray();
+            array2 = array2.Reverse().ToArray();
+
+            int reversedCounter = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (array1[i] == array2[i])
+                {
+                    reversedCounter++;
+                }
+            }
+
+            if (counter > reversedCounter)
+            {
+                Console.WriteLine(counter);
+            }
+            else
+            {
+                Console.WriteLine(reversedCounter);
+            }
+        }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/05ArraysExercises/02RotateSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02RotateSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rotations = int.Parse(Console.ReadLine());
+            int[] sum = new int[inputNumbers.Length];
+            for (int i = 0; i < rotations; i++)
+            {
+                int lastDigit = inputNumbers[inputNumbers.Length - 1];
+                for (int j = inputNumbers.Length - 1 ; j > 0; j--)
+                {
+                    inputNumbers[j] = inputNumbers[j - 1];
+                    sum[j] += inputNumbers[j];
+                }
+                inputNumbers[0] = lastDigit;
+                sum[0] += inputNumbers[0];
+            }
+
+            for (int i = 0; i < sum.Length; i++)
+            {
+                Console.Write(sum[i] + " ");
+            }
+        }
+    }
+}
