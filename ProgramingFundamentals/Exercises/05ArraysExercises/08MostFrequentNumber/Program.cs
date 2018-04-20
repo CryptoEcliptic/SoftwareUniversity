7d3af555763a63a169ae34330d28bd1a619665c6
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-20 15:51:51 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/05ArraysExercises/08MostFrequentNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08MostFrequentNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int totalSequence = 0;
+            int result = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+                int currentSequence = 0;
+
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (currentNumber == numbers[j])
+                    {
+                        currentSequence++;
+                        if (currentSequence > totalSequence)
+                        {
+                            totalSequence = currentSequence;
+                            result = currentNumber;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/05ArraysExercises/09IndexOfLetters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09IndexOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            char[] alphabet = new char[26];
+            int counter = 0;
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                alphabet[counter] = i;
+                counter++;
+            }
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char currentLetter = word[i];
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (currentLetter == alphabet[j])
+                    {
+                        Console.WriteLine($"{currentLetter} -> {j}");
+                    }
+                }
+            }
+        }
+    }
+}
