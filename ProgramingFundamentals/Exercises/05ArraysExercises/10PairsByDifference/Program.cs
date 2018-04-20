03f1d921583f7f4edd02329caf6cdd1ad6ba9959
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-20 15:53:44 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/05ArraysExercises/10PairsByDifference/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10PairsByDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int difference = int.Parse(Console.ReadLine());
+
+            int result = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+                for (int j = i; j < numbers.Length; j++)
+                {
+
+                    if (Math.Abs(currentNumber - numbers[j]) == difference)
+                    {
+                        result++;
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/05ArraysExercises/11EqualSums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11EqualSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            bool hasEqualsum = false;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int[] firstHalf = numbers.Take(i).ToArray();
+                int[] secondHalf = numbers.Skip(i + 1).ToArray();
+                if (firstHalf.Sum() == secondHalf.Sum())
+                {
+                    hasEqualsum = true;
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+            if (!hasEqualsum)
+            {
+                Console.WriteLine("no");
+
+            }
+        }
+    }
+}
