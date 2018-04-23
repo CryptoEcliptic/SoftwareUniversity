827c91785817c0a9b1eabeda7b06b126963140db
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-23 17:32:34 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/06Lists/01RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            List<int> positiveValues = new List<int>();
+            int countNumber = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] >= 0)
+                {
+                    positiveValues.Add(numbers[i]);
+                    countNumber++;
+                }
+            }
+            positiveValues.Reverse();
+            if (countNumber < 1)
+            {
+                Console.WriteLine("empty");
+            }
+            Console.WriteLine(string.Join(" ", positiveValues));
+            
+        }
+    }
+}
---
+++ ProgramingFundamentals/Lab/06Lists/02AppendLists/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02AppendLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split('|').ToList();
+            input.Reverse();
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+    }
+}
