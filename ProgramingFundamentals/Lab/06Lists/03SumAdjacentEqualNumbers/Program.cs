24ec2e8afae5b3abacb2efd183072340a827ec1f
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-23 17:33:53 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/06Lists/03SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> inputNumbers = Console.ReadLine().Split().Select(decimal.Parse).ToList();
+
+            for (int i = 1; i < inputNumbers.Count; i++)
+            {
+                if (inputNumbers[i] == inputNumbers[i - 1])
+                {
+                    inputNumbers[i] = inputNumbers[i] + inputNumbers[i - 1]; //i(1 position) == i(1 position) + i-1(0 position)
+                    inputNumbers.Remove(inputNumbers[i - 1]); //Position i(1) becomes i-1 e.g. 0;
+                    i = 0;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", inputNumbers));
+        }
+    }
+}
---
+++ ProgramingFundamentals/Lab/06Lists/04SplitWordByCasing/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04SplitWordByCasing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine()
+               .Split(new char[] { ',', ';', ' ', '.', ':', '!', '(', ')', '"', '\'', '\\', '/', '[', ']' },
+               StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<string> lowerCaseWords = new List<string>();
+            List<string> mixedCaseWords = new List<string>();
+            List<string> upperCaseWords = new List<string>();
+
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                if (words[i].All(char.IsLower))
+                {
+                    lowerCaseWords.Add(words[i]);
+                }
+                else if (words[i].All(char.IsUpper))
+                {
+                    upperCaseWords.Add(words[i]);
+                }
+                else
+                {
+                    mixedCaseWords.Add(words[i]);
+                }
+            }
+            Console.WriteLine($"Lower-case: {string.Join(", ", lowerCaseWords)}");
+            Console.WriteLine($"Mixed-case: {string.Join(", ", mixedCaseWords)}");
+            Console.WriteLine($"Upper-case: {string.Join(", ", upperCaseWords)}");
+        }
+    }
+}
