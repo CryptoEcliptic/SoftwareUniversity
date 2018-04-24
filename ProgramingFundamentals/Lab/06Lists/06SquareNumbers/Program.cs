422d944f0c6e88cbaddf6c985d63b69ea93edfd3
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-24 19:00:31 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/06Lists/06SquareNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06SquareNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < inputNumbers.Count; i++)
+            {
+                int currentNumber = inputNumbers[i];
+                var square = Math.Sqrt(currentNumber);
+                if (square == (int)square)
+                {
+                    result.Add((int)currentNumber);
+                }
+            }
+            result.Sort();
+            result.Reverse();
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
