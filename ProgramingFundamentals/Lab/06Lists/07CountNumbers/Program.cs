f182327fca1472a5c66957c16b3c794cd4a662f5
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-24 19:01:36 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/06Lists/07CountNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07CountNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int[] countNumber = new int[1001];
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int currentNumber = numbers[i];
+                countNumber[currentNumber]++;
+            }
+
+            for (int i = 0; i < countNumber.Length; i++)
+            {
+                if (countNumber[i] > 0)
+                {
+                    Console.WriteLine($"{i} -> {countNumber[i]}");
+                }
+            }
+        }
+    }
+}
