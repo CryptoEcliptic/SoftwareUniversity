e8c8be813a2eaeebd682adec916074357e885f3a
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-25 16:55:00 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/06Lists/01MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            int currentLenght = 1;
+            int currentStart = 0;
+            int maxLength = 1;
+            int maxStart = 0;
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    currentLenght++;
+                    if (currentLenght > maxLength)
+                    {
+                        maxLength = currentLenght;
+                        maxStart = currentStart;
+                    }
+                }
+                else
+                {
+                    currentStart = i;
+                    currentLenght = 1;
+                }
+            }
+            for (int i = maxStart; i < maxStart + maxLength; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
