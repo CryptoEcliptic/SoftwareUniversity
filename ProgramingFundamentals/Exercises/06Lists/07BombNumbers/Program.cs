e1642ac2621763b91c064a4e3095ba1a1daa889d
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-27 15:08:14 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/06Lists/07BombNumbers/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07BombNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputNumbers = Console.ReadLine()
+           .Split(' ')
+           .Select(int.Parse)
+           .ToList();
+
+            int[] bombNumbers = Console.ReadLine()
+           .Split(' ')
+           .Select(int.Parse)
+           .ToArray();
+
+            int bomb = bombNumbers[0];
+            int range = bombNumbers[1];
+
+            for (int i = 0; i < inputNumbers.Count; i++)
+            {
+                if (inputNumbers[i] == bomb)
+                {
+                    if (range <= i)
+                    {
+                        for (int j = i; j >= i - range; j--)
+                        {
+                            inputNumbers[j] = 0;
+                        }
+                    }
+                    else if (range > i)
+                    {
+                        for (int j = i; j >= 0; j--)
+                        {
+                            inputNumbers[j] = 0;
+                        }
+                    }
+                    if (range + i > inputNumbers.Count)
+                    {
+                        for (int j = i; j < inputNumbers.Count; j++)
+                        {
+                            inputNumbers[j] = 0;
+                        }
+                    }
+                    else if (range + i <= inputNumbers.Count)
+                    {
+                        for (int j = i; j <= i + range ; j++)
+                        {
+                            inputNumbers[j] = 0;
+                        }
+                    }  
+                }
+            }
+            Console.WriteLine(inputNumbers.Sum());
+        }
+    }
+}
