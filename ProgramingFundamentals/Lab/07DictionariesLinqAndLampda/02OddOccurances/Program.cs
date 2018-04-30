38c03f45684df8aa793b39abe730a479434ba852
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-30 16:33:13 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/07DictionariesLinqAndLampda/02OddOccurances/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02OddOccurances
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputText = Console.ReadLine()
+                .ToLower()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Dictionary<string, int> wordOccurances = new Dictionary<string, int>();
+
+            foreach (var word in inputText) // Taking data from the input and put it into Dictionary wordOccurances
+            {
+                if (wordOccurances.ContainsKey(word))
+                {
+                    wordOccurances[word]++;
+                }
+                else
+                {
+                    wordOccurances[word] = 1;
+                }
+            }
+            var result = new List<string>();
+            foreach (var kvp in wordOccurances)
+            {
+                string word = kvp.Key;
+                int times = kvp.Value;
+                if (times % 2 == 1)
+                {
+                    result.Add(word);
+                }
+            }
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
