292639b4e476fe2257cd532b6855002387e611a9
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-04-30 16:31:31 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/07DictionariesLinqAndLampda/01CountRealNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputNumbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> numbersCount = new SortedDictionary<double, int>();
+
+            foreach (var number in inputNumbers)
+            {
+                if (!numbersCount.ContainsKey(number))
+                {
+                    numbersCount[number] = 0;
+                }
+
+                    numbersCount[number]++;  
+            }
+            foreach (var kvp in numbersCount)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
