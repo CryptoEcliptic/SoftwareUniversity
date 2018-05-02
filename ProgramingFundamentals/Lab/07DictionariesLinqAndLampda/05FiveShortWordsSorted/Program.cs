f4fe92134234c5912ad7f18c8ac4ad844ca5e1d7
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-02 17:07:36 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/07DictionariesLinqAndLampda/05FiveShortWordsSorted/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05FiveShortWordsSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputWords = Console.ReadLine()
+                .Split(new char[] { '.', ',', ':', ';', '(', ')', '[', ']', '\"', '\'', '\\', '/', '!', '?', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => w.Length < 5)
+                .Select(w => w.ToLower())
+                .OrderBy(w => w)
+                .Distinct()
+                .ToList();
+            Console.Write(string.Join(", ", inputWords));
+            Console.WriteLine();
+        }
+    }
+}
