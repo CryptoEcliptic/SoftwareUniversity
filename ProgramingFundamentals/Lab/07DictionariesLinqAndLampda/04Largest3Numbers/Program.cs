e9fdeee095283ff1cce14b45b172d259a463f6ec
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-02 17:06:22 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/07DictionariesLinqAndLampda/04Largest3Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> inputNumbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            inputNumbers = inputNumbers.OrderByDescending(x => x).Take(3).ToList();
+            foreach (var number in inputNumbers)
+            {
+                double result = number;
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
