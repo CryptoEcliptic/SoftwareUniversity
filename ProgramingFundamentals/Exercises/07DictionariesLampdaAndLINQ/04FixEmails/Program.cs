a4e4177100ed78f83b4e465ffcfceb81d7b4f930
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-03 17:08:48 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/07DictionariesLampdaAndLINQ/04FixEmails/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04FixEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> emailList = new Dictionary<string, string>();
+            string command = Console.ReadLine();
+            string name = null;
+            while (command != "stop")
+            {
+                name = command;
+                string email = Console.ReadLine();
+                if (!emailList.ContainsKey(name))
+                {
+                    emailList.Add(name, email);
+                }
+                command = Console.ReadLine();
+            }
+            foreach (var kvp in emailList)
+            {
+                if (!(kvp.Value.EndsWith("us") || kvp.Value.EndsWith("uk")))
+                {
+                    Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+                }
+            }
+        }
+    }
+}
