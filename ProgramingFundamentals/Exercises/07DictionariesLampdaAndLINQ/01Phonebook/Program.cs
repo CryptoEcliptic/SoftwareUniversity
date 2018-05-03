e4b92687f61a2c89b7ce429536ac909e029e7748
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-03 17:05:43 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/07DictionariesLampdaAndLINQ/01Phonebook/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                 .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                 .ToArray();
+
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+            while (input[0] != "END")
+            {
+
+                if (input[0] == "A")
+                {
+                    if (!phonebook.ContainsKey(input[1]))
+                    {
+                        phonebook.Add(input[1], input[2]);
+                    }
+                    else if (phonebook.ContainsKey(input[1]))
+                    {
+                        phonebook[input[1]] = input[2];
+                    }
+                }
+                else if (input[0] == "S")
+                {
+                    if (!phonebook.ContainsKey(input[1]))
+                    {
+                        Console.WriteLine($"Contact {input[1]} does not exist.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{input[1]} -> {phonebook[input[1]]}");
+                    }
+                }
+
+                input = Console.ReadLine()
+               .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+               .ToArray();
+            }
+        }
+    }
+}
