bcfa5d51b5ea7a2ace37c42f8e3b29e34dece888
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-08 17:49:44 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/08ObjectsAndClasses/01DayOfWeek/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace _01DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateInput = Console.ReadLine();
+            DateTime date = DateTime.ParseExact(dateInput, "d-M-yyyy", CultureInfo.InvariantCulture);
+
+            Console.WriteLine(date.DayOfWeek);  
+        }
+    }
+}
