39a32cfb23ea10f265d74d6c8b71240bb9c68967
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-08 17:50:35 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/08ObjectsAndClasses/02RandomizeWords/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputWords = Console.ReadLine().Split(' ').ToArray();
+            var random = new Random();
+
+            for (int i = 0; i < inputWords.Length; i++)
+            {
+                var currentWord = inputWords[i];
+                var randomIndex = random.Next(0, inputWords.Length);
+                var randomWord = inputWords[randomIndex];
+                inputWords[i] = randomWord;
+                inputWords[randomIndex] = currentWord;
+            }
+            foreach (var words in inputWords)
+            {
+                Console.WriteLine(words);
+            }
+        }
+    }
+}
