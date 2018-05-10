d8bdf4d6636784e10a28c44394c9ce000a3df3f2
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-10 15:30:59 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/08ObjectsAndClasses/06RectanglePosition/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06RectanglePosition
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = ReadRectangle();
+            var second = ReadRectangle();
+            var result = first.IsInside(second);
+            if (result == true)
+            {
+                Console.WriteLine("Inside");
+            }
+            else
+            {
+                Console.WriteLine("Not inside");
+            }
+          
+           
+        }
+       static Rectangle ReadRectangle()
+        {
+            var rectData = Console.ReadLine().Split(' ');
+
+            return new Rectangle
+            {
+                Left = int.Parse(rectData[0]),
+                Top = int.Parse(rectData[1]),
+                Width = int.Parse(rectData[2]),
+                Height = int.Parse(rectData[3])
+            };
+        }
+    }
+}
---
+++ ProgramingFundamentals/Lab/08ObjectsAndClasses/06RectanglePosition/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06RectanglePosition
+{
+    class Rectangle
+    {
+        public int Top { get; set; }
+        public int Left { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int Bottom
+        {
+            get
+            {
+                return Top + Height;
+            }
+        }
+        public int Right
+        {
+            get
+            {
+                return Left + Width;
+            }
+        }
+        public int Area
+        {
+            get
+            {
+                return Width * Height;
+            } 
+        }
+        public bool IsInside(Rectangle other)
+        {
+            return Left >= other.Left && Right <= other.Right && Bottom <= other.Bottom && Top >= other.Top;
+        }
+    }
+}
