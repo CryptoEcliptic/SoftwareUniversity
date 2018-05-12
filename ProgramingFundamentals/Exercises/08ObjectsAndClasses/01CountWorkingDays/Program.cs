23fdec7957e2ce0a56fa720ad7b1f6432d409476
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-12 13:11:22 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/08ObjectsAndClasses/01CountWorkingDays/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace _01CountWorkingDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startDate = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            int counter = 0;
+            bool isHoliday = false;
+
+            List<DateTime> holidays = new List<DateTime>()
+            {
+            DateTime.ParseExact("01-01-1970", "dd-MM-yyyy", CultureInfo.InvariantCulture),
+            DateTime.ParseExact("03-03-1970", "dd-MM-yyyy", CultureInfo.InvariantCulture),
+            DateTime.ParseExact("01-05-1970", "dd-MM-yyyy", CultureInfo.InvariantCulture),
+            DateTime.ParseExact("06-05-1970", "dd-MM-yyyy", CultureInfo.InvariantCulture),
+            DateTime.ParseExact("24-05-1970", "dd-MM-yyyy", CultureInfo.InvariantCulture),
+            DateTime.ParseExact("06-09-1970", "dd-MM-yyyy", CultureInfo.InvariantCulture),
+            DateTime.ParseExact("22-09-1970", "dd-MM-yyyy", CultureInfo.InvariantCulture),
+            DateTime.ParseExact("01-11-1970", "dd-MM-yyyy", CultureInfo.InvariantCulture),
+            DateTime.ParseExact("24-12-1970", "dd-MM-yyyy", CultureInfo.InvariantCulture),
+            DateTime.ParseExact("25-12-1970", "dd-MM-yyyy", CultureInfo.InvariantCulture),
+            DateTime.ParseExact("26-12-1970", "dd-MM-yyyy", CultureInfo.InvariantCulture)
+        };
+            
+            for (DateTime day = startDate; day <= endDate; day = day.AddDays(1)) //Adding days to a dateTime.
+            {
+               
+                if (day.DayOfWeek == DayOfWeek.Saturday || day.DayOfWeek == DayOfWeek.Sunday) // Check if a DateTime is Saturday/Sunday.
+                {
+                    isHoliday = true;
+                }
+                else
+                {
+                    for (int i = 0; i < holidays.Count; i++)
+                    {
+                        if (day.Month == holidays[i].Month && day.Day == holidays[i].Day)
+                        {
+                            isHoliday = true;
+                        }
+                    }
+                }
+                
+                if (!isHoliday)
+                {
+                    counter++;
+                }
+                isHoliday = false;
+            }
+            
+            Console.WriteLine(counter);
+
+        }
+    }
+}
