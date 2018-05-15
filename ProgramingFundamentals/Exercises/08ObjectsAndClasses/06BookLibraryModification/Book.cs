379166e079f36a736a56a9674e2e04b7eaa8cae2
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-15 15:55:29 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/08ObjectsAndClasses/06BookLibraryModification/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06BookLibraryModification
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Autor { get; set; }
+        public string Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string IBSNnumber { get; set; }
+        public double Price { get; set; }
+    }
+    class Library
+    {
+        public string Name { get; set; }
+        public List<Book> ListBooks { get; set; }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/08ObjectsAndClasses/06BookLibraryModification/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06BookLibraryModification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberLines = int.Parse(Console.ReadLine());
+            List<Book> bookList = new List<Book>();
+
+            for (int i = 0; i < numberLines; i++)
+            {
+                List<string> input = Console.ReadLine().Split(' ').ToList();
+                Book inputBook = new Book();
+                inputBook.Title = input[0];
+                inputBook.Autor = input[1];
+                inputBook.Publisher = input[2];
+                inputBook.ReleaseDate = DateTime.ParseExact(input[3], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                inputBook.IBSNnumber = input[4];
+                inputBook.Price = double.Parse(input[5]);
+                bookList.Add(inputBook);
+            }
+            DateTime givenDate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            Library MyLibrary = new Library();
+            MyLibrary.ListBooks = bookList;
+
+            foreach (var pair in MyLibrary.ListBooks.Where(x => x.ReleaseDate > givenDate).OrderBy(x => x.ReleaseDate).ThenBy(x => x.Title))
+            {
+                Console.WriteLine($"{pair.Title} -> {pair.ReleaseDate.ToString("dd.MM.yyyy")}");
+            }
+        }
+    }
+}
