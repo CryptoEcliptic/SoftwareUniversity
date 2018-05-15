7a0b49063e14142deac545fb338d678b5156f028
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-15 15:56:16 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/08ObjectsAndClasses/09TeamworkProjects/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09TeamworkProjects
+{
+    class Team
+    {
+        public string Name { get; set; }
+        public List<string> Members { get; set; }
+        public string Creator { get; set; }
+    }
+}
