2e3774b9f2a428de1a798018c4ff675665202a4e
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-16 20:08:06 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/09StringAndTextProcessing/01ReverseString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            char[] convertedText = inputText.ToCharArray();
+            Array.Reverse(convertedText);
+
+            Console.WriteLine(convertedText);
+        }
+    }
+}
