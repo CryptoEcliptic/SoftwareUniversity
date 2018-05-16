3e141e2bbb529395ab1f4e9e4405df9f65920077
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-16 20:09:02 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/09StringAndTextProcessing/02CountSubstringOccurances/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02CountSubstringOccurances
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine().ToLower();
+            string searchedWord = Console.ReadLine().ToLower();
+            int counter = 0;
+            int combinations = inputText.IndexOf(searchedWord);
+
+            while (combinations != -1)
+            {
+                counter++;
+                combinations = inputText.IndexOf(searchedWord, combinations+1);
+            }  
+                
+            Console.WriteLine(counter);
+        }
+    }
+}
