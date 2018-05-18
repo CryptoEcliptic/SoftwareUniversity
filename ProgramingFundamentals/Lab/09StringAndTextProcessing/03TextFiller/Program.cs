fd0c346eebbe384fb0243f3d6e8f6e1097029975
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-18 15:48:29 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/09StringAndTextProcessing/03TextFiller/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03TextFiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string inputText = Console.ReadLine();
+
+            foreach (var word in bannedWords)
+            {
+                var stars = new string('*', word.Length);
+                inputText = inputText.Replace(word, stars);
+            }
+            Console.WriteLine($"{inputText}");
+        }
+    }
+}
