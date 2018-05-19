26477735851cfb9772fde54302531fbe8ad750a0
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-19 15:53:59 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/09StringAndTextProcessing/01ConvertFromBase10ToBaseN/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01ConvertFromBase10ToBaseN
+{
+    class Program
+    {
+  
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+            int baseN = int.Parse(input[0]);
+            BigInteger base10Number = new BigInteger();
+            base10Number = BigInteger.Parse(input[1]);
+            BigInteger reminder = new BigInteger();
+            string result = string.Empty;
+
+            while (base10Number > 0)
+            {
+                reminder = base10Number % baseN;
+                base10Number = base10Number / baseN;
+                result = reminder.ToString() + result;
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
