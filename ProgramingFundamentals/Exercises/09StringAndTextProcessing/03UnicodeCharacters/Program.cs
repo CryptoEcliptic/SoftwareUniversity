bc30b55f32d6d1d6941a63833443ee40dc9bedde
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-20 17:38:38 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/09StringAndTextProcessing/03UnicodeCharacters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03UnicodeCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int result = 0;
+            char[] arr = input.ToCharArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result = arr[i];
+                Console.Write("\\u{0:x4}", result);
+            }
+            Console.WriteLine();
+        }
+    }
+}
