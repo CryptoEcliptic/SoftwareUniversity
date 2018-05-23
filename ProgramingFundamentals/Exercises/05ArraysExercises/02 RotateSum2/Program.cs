60a07ecbbcc12606bdc5aa13674a61d3fb162fcc
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-23 17:32:56 +0300

    Using List

---
+++ ProgramingFundamentals/Exercises/05ArraysExercises/02 RotateSum2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_RotateAndSumWithList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int[] sum = new int[input.Count];
+
+            int k = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < k; i++)
+            {
+                input.Insert(0, input[input.Count - 1]);
+                input.RemoveAt(input.Count - 1);
+
+                for (int j = 0; j < input.Count; j++)
+                {
+                    sum[j] = sum[j] + input[j];
+                }
+            }
+            Console.WriteLine(string.Join(" ", sum));
+        }
+    }
+}
