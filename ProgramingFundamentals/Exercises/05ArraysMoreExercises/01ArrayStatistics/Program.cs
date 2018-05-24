d0bf00202cd5c4da26b11df8b986b2bd71ac03c5
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-24 12:56:17 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/05ArraysMoreExercises/01ArrayStatistics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _01ArrayStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine()
+                 .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+            Console.WriteLine($"Min = {inputNumbers.Min()}");
+            Console.WriteLine($"Max = {inputNumbers.Max()}");
+            Console.WriteLine($"Sum = {inputNumbers.Sum()}");
+            Console.WriteLine($"Average = {inputNumbers.Average()}");
+        }
+    }
+}
