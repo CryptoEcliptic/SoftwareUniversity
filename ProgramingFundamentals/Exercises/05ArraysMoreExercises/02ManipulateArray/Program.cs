54e88f2d77251253b9fb77eb9012fc050973ea20
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-24 12:57:42 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/05ArraysMoreExercises/02ManipulateArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _02ManipulateArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputText = Console.ReadLine().Split(' ').ToArray();
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string commands = Console.ReadLine();
+
+                if (commands == "Distinct")
+                {
+                    inputText = inputText.Distinct().ToArray();
+                }
+                else if (commands == "Reverse")
+                {
+                    Array.Reverse(inputText);
+                }
+                else
+                {
+                    string[] replace = commands.Split(' ').ToArray(); //easy replacement
+
+                    inputText[int.Parse(replace[1])] = replace[2];
+                }
+            }
+            Console.WriteLine(string.Join(", ", inputText));
+        }
+    }
+}
