5948bd32b057b5942d141c6b62ab6dd2e5f8f425
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-26 15:34:53 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/05ArraysMoreExercises/04GrabAndGo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _04GrabAndGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int keyNumber = int.Parse(Console.ReadLine());
+            int countNumbers = 0;
+            
+            long sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == keyNumber)
+                {
+                    countNumbers = i;
+                }
+            }
+            for (int i = 0; i < countNumbers; i++)
+            {
+                sum += input[i];
+            }
+            if (countNumbers >= 1)
+            {
+                Console.WriteLine(sum);
+            }
+            else if (countNumbers == 0)
+            {
+                Console.WriteLine("No occurrences were found!");
+            }
+
+        }
+    }
+}
+    
