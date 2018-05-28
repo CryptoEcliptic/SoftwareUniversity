598d1c8b4fe6bf8d16158c3eed7552ddd3bbd2bc
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-28 15:58:21 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/05ArraysMoreExercises/07InventoryMatcher/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _07InventoryMatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] products = Console.ReadLine()
+               .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+               .ToArray();
+
+            long[] quantity = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToArray();
+            decimal[] price = Console.ReadLine()
+                 .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(decimal.Parse)
+                 .ToArray();
+            string input = Console.ReadLine();
+            while (input != "done")
+            {
+                for (int i = 0; i < products.Length; i++)
+                {
+                    if (input == products[i])
+                    {
+                        Console.WriteLine($"{products[i]} costs: {price[i]}; Available quantity: {quantity[i]}");
+                    }
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
