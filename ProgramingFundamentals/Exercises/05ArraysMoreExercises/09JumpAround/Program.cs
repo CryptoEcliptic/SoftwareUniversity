b2eaa93c87e8c2696a6f47a5387cb022dd857bfd
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-29 16:46:39 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/05ArraysMoreExercises/09JumpAround/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _09JumpAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int position = 0;
+            int sum = input[0];
+            int currentIndex = input[0];
+
+            while (true)
+            {
+                if (position + currentIndex < input.Length)
+                {
+                    position += currentIndex;
+                }
+                else if (position - currentIndex >= 0)
+                {
+                    position = position - currentIndex;
+                }
+                else
+                {
+                    break;
+                }
+                sum += input[position];
+                currentIndex = input[position];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
