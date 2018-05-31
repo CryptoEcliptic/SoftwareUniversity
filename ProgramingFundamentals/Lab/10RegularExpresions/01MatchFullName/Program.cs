56a3d81ee0b27cc61e9975e8b280f4562ace2d3e
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-05-31 11:17:11 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/10RegularExpresions/01MatchFullName/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            MatchCollection patternResult = Regex.Matches(input, pattern);
+
+            foreach (Match result in patternResult)
+            {
+                Console.Write($"{result.Value} ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
