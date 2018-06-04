c5f394545afe39d67c6075802af5ea1fba1ef8db
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-06-04 16:03:48 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/10RegularExpresions/05MatchNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _05MatchNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))";
+            MatchCollection numbers = Regex.Matches(input, pattern);
+
+            var toPrint = numbers
+                .Cast<Match>()
+                .Select(x => x.Value.Trim())
+                .ToArray();
+            Console.WriteLine(string.Join(" ", toPrint));
+        }
+    }
+}
