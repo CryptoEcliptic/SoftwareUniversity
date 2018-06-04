a26a0df6894eda55547bee6b8caa71597f96d7c2
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-06-04 16:02:35 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/10RegularExpresions/04MatchDates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _04MatchDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputDate = Console.ReadLine();
+            var pattern = @"\b(?<date>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+
+            var formattedDate = Regex.Matches(inputDate, pattern);
+
+            foreach (Match print in formattedDate)
+            {
+                var day = print.Groups["date"].Value;
+                var month = print.Groups["month"].Value;
+                var year = print.Groups["year"].Value;
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
