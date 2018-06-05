a37a4d233f085d6a4f8e1d1729909e19e8125fed
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-06-05 15:57:16 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/10RegularExpressions/01ExtractEmails/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputEmails = Console.ReadLine();
+            string pattern = @"(^| )[A-Za-z0-9][A-Za-z0-9.\-_]*@[A-Z-a-z-]+(\.[a-z]+)+";
+            MatchCollection emails = Regex.Matches(inputEmails, pattern);
+
+            foreach (Match mail in emails)
+            {
+                Console.WriteLine(mail.Value.Trim());
+            }
+        }
+    }
+}
