2693892ae56da8f73bb5d375483d13dc39a99f00
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-06-05 15:54:55 +0300

    Lab exercises

---
+++ ProgramingFundamentals/Lab/10RegularExpresions/06ReplaceTag/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _06ReplaceTag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+
+            while (inputText != "end")
+            {
+                string pattern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+                string replacement = @"[URL href=$1]$2[/URL]";
+                string replacedText = Regex.Replace(inputText, pattern, replacement);
+
+                Console.WriteLine(replacedText);
+
+                inputText = Console.ReadLine();
+            }
+        }
+    }
+}
