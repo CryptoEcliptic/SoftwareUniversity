6ad36c8522897d0d9c729ebb3552adf7ae8f672d
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-06-07 14:09:01 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/10RegularExpressions/02ExtractSentencesByKeyword/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _02ExtractSentencesByKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string keyword = Console.ReadLine();
+            string[] inputText = Console.ReadLine()
+                .Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            foreach (var sentence in inputText)
+            {
+                var words = Regex.Split(sentence, @"[^A-Za-z0-9]+");
+                foreach (var word in words)
+                {
+                    if (word == keyword)
+                    {
+                        Console.WriteLine(sentence.Trim());
+                        break;
+                    }
+                }
+            }
+           
+        }
+    }
+}
