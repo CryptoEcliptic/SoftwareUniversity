dd8ad8c236797e700397d8c4d794e167b0bac8c0
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-06-09 14:11:58 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/10RegularExpressions/06ValidUsernames/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _06ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputNames = Console.ReadLine();
+            string pattern = @"\b[A-Za-z]\w{2,24}\b";
+            MatchCollection namesInPattern = Regex.Matches(inputNames, pattern);
+            int maxLength = 0;
+            string currentMaxLengthUsernames = "";
+
+            for (int i = 0; i < namesInPattern.Count - 1; i++)
+            {
+                Match firstName = namesInPattern[i];
+                Match secondName = namesInPattern[i + 1];
+                int sum = firstName.Length + secondName.Length;
+                if (sum > maxLength)
+                {
+                    maxLength = sum;
+                    currentMaxLengthUsernames = "";
+                    currentMaxLengthUsernames = firstName + "\n" + secondName;
+                }
+            }
+            Console.WriteLine(currentMaxLengthUsernames);
+           
+        }
+    }
+}
