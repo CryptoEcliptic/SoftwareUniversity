2cc1e697babc1dad5d744c3becc2839f50d1da97
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-06-11 16:54:25 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/07DictionariesAndLampdaMoreExercises/01SortTimes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _01SortTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] time = Console.ReadLine().Split(' ').ToArray();
+
+            var timeSorted = time.OrderBy(x => x).ToArray();
+            Console.WriteLine(string.Join(", ", timeSorted));
+        }
+    }
+}
