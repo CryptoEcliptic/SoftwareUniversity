748bb0e22049845626dd74b43af73d157bfb9c46
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-06-12 09:02:24 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/07DictionariesAndLampdaMoreExercises/02OddFilter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _02OddFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] managedNums = inputNumbers
+                .Where(x => x % 2 == 0)
+                .ToArray();
+            double average = managedNums.Average();
+
+            for (int i = 0; i < managedNums.Length; i++)
+            {
+                if (managedNums[i] > average)
+                {
+                    managedNums[i] += 1;
+                }
+                else
+                {
+                    managedNums[i] -= 1;
+                }
+                Console.Write(managedNums[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
