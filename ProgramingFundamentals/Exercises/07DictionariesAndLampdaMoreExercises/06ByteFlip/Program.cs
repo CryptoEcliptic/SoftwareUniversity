5a4d747b6f91f00572cffcc635b558ca8cac7480
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-06-16 11:40:24 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/07DictionariesAndLampdaMoreExercises/06ByteFlip/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _06ByteFlip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                 .Split(' ')
+                 .Where(x => x.Length == 2)
+                 .ToArray();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                char[] reversed = input[i].ToCharArray(); // converting the input To char[]
+                Array.Reverse(reversed); // reversing the char[]
+                Console.Write(System.Convert.ToChar(System.Convert.ToUInt32(new string(reversed), 16)));
+            }
+            Console.WriteLine();
+        }
+    }
+}
