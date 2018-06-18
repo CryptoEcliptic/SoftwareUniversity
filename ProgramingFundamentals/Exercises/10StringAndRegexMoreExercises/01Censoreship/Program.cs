31a6cdf2ba1d34fdb097ed140b50bf6865250e59
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-06-18 07:51:45 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/10StringAndRegexMoreExercises/01Censoreship/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01Censoreship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string input = Console.ReadLine();
+            string result = input.Replace(word, new string('*', word.Length));
+            Console.WriteLine(result);
+        }
+    }
+}
