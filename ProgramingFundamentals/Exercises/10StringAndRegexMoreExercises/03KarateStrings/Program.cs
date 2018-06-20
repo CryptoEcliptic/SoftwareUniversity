a1075e603deb2a4afcfd734e1ee809d3cc98ef4c
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-06-20 19:02:16 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/10StringAndRegexMoreExercises/03KarateStrings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03KarateStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> input = Console.ReadLine().ToCharArray().ToList(); ;
+            int strength = 0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] == '>')
+                {
+                    strength += int.Parse(input[i + 1].ToString());
+                }
+                else
+                {
+                    if (strength > 0)
+                    {
+                        input.RemoveAt(i);
+
+                        strength--;
+                        i--;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join("", input));
+        }
+    }
+}
