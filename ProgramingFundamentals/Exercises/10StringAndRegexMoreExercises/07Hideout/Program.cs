bf885d7cca4ed9f5e41490222265bc49e00109ed
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-06-21 16:11:29 +0300

    Exercises

---
+++ ProgramingFundamentals/Exercises/10StringAndRegexMoreExercises/07Hideout/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _07Hideout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool hasFound = false;
+            int maxCount = 0;
+            int maxPosition = 0;
+            while (true)
+            {
+                if (hasFound == true)
+                {
+                    break;
+                }
+                string[] elements = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                char searchedSymbol = char.Parse(elements[0]);
+                int number = int.Parse(elements[1]);
+
+                int position = 0;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] == searchedSymbol)
+                    {
+                        position = i;
+                        int actualCount = 0;
+                        for (int j = i; j < input.Length; j++)
+                        {
+                            if (input[j] != searchedSymbol)
+                            {
+                                break;
+                            }
+                            actualCount++;
+                            if (actualCount > maxCount)
+                            {
+                                maxCount = actualCount;
+                                maxPosition = position;
+                            }
+                            if (maxCount >= number)
+                            {
+                                hasFound = true;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Hideout found at index {maxPosition} and it is with size {maxCount}!");
+        }
+    }
+}
