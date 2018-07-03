021c70de30de3b416f586f08257fac895db929c3
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-07-03 07:40:35 +0300

    Exam problem 3

---
+++ ProgramingFundamentals/Exam Problems/Exam 26 Feb 2017/03HornetAssault/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03HornetAssault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<long> beehives = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToList();
+
+            List<long> hornets = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToList();
+
+            long minIndex = Math.Min(beehives.Count, hornets.Count);
+            long sumHornets = hornets.Sum();
+
+
+            for (int i = 0; i < minIndex; i++)
+            {
+                if (i > beehives.Count - 1)
+                {
+                    break;
+                }
+                if (hornets.Count == 0) //Important for incorrect input. 6-th test judge fail without that code part
+                {
+                    break;
+                }
+                if (beehives.Count == 0) // same, but no test for it in judge
+                {
+                    break;
+                }
+                if (sumHornets > beehives[i]) 
+                {
+                    beehives.RemoveAt(i);
+                    i--;
+                }
+                else if (beehives[i] >= sumHornets)
+                {
+                    beehives[i] = beehives[i] - sumHornets;
+                    if (beehives[i] == 0)
+                    {
+                        beehives.RemoveAt(i);
+                        i--;
+                    }
+                    hornets.RemoveAt(0);
+                    sumHornets = hornets.Sum();
+                }
+            }
+            long sumBees = beehives.Sum();
+
+            if (sumBees > 0)
+            {
+                Console.WriteLine(string.Join(" ", beehives));
+            }
+            else if(sumBees < 1)
+            {
+                Console.WriteLine(string.Join(" ", hornets));
+            }
+        }
+    }
+}
