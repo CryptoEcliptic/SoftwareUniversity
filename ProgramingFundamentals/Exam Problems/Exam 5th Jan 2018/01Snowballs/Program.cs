09fe489a549f9ed23bd0f7b8f2f1b808497fbc56
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-07-11 16:28:08 +0300

    Exam problem 1

---
+++ ProgramingFundamentals/Exam Problems/Exam 5th Jan 2018/01Snowballs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+namespace _01Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger snowballValue = -999999999999;
+            int snow = 0;
+            int time = 0;
+            int quality = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                BigInteger snowballValueCurrent = BigInteger.Pow(snowballSnow / snowballTime, snowballQuality);
+
+                if (snowballValueCurrent > snowballValue)
+                {
+                    snowballValue = snowballValueCurrent;
+                    snow = snowballSnow;
+                    time = snowballTime;
+                    quality = snowballQuality;
+                }
+            }
+            Console.WriteLine($"{snow} : {time} = {snowballValue} ({quality})");
+        }
+    }
+}
