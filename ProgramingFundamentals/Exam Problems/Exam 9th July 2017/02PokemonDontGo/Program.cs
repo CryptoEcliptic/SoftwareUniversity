115536ea96ace315dae187177ec3b2572d1ed843
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-07-12 17:30:54 +0300

    Exam problem 2

---
+++ ProgramingFundamentals/Exam Problems/Exam 9th July 2017/02PokemonDontGo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02PokemonDontGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputSequence = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<int> result = new List<int>();
+            int totalSum = 0;
+            int elementKey = 0;
+
+            while (inputSequence.Count > 0)
+            {
+
+                int indexes = int.Parse(Console.ReadLine());
+
+                if (indexes < 0)
+                {
+                    elementKey = inputSequence[0];
+                    result.Add(inputSequence[0]);
+                    inputSequence[0] = inputSequence[inputSequence.Count - 1];
+                }
+                else if (indexes > inputSequence.Count - 1)
+                {
+                    elementKey = inputSequence[inputSequence.Count - 1];
+                    result.Add(inputSequence[inputSequence.Count - 1]);
+                    inputSequence[inputSequence.Count - 1] = inputSequence[0];
+                }
+
+                else
+                {
+                    elementKey = inputSequence[indexes];
+                    result.Add(inputSequence[indexes]);
+                    inputSequence.RemoveAt(indexes);
+                }
+
+                for (int i = 0; i < inputSequence.Count; i++)
+                {
+                    if (inputSequence[i] <= elementKey)
+                    {
+                        inputSequence[i] += elementKey;
+                    }
+                    else if (inputSequence[i] > elementKey)
+                    {
+                        inputSequence[i] = inputSequence[i] - elementKey;
+                    }
+                }
+            }
+            for (int i = 0; i < result.Count; i++)
+            {
+                totalSum += result[i];
+            }
+            Console.WriteLine(totalSum);
+        }
+    }
+}
