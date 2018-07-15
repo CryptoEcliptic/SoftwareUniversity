18d99bb71c93fd048540bb12597e20de98e05765
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-07-15 10:28:03 +0300

    Exam problem 1

---
+++ ProgramingFundamentals/Exam Problems/Exam 6th Jan 2017/01SinoTheWalker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01SinoTheWalker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.ParseExact(Console.ReadLine(), "HH:mm:ss", CultureInfo.InstalledUICulture);
+            int numberSteps = int.Parse(Console.ReadLine()) % 86400; // if the input is more than a day. 1 day holds 86400 seconds
+            int stepTime = int.Parse(Console.ReadLine()) % 86400;
+
+            long travelTimeSeconds = numberSteps * stepTime;
+
+            string arrivalTime = time.AddSeconds(travelTimeSeconds).ToString("HH:mm:ss");
+            Console.WriteLine($"Time Arrival: {arrivalTime}");
+        }
+    }
+}
