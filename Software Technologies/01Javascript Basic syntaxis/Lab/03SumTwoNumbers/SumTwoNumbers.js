f6055c0a29fb037588de87f0ac1e51c0a3c0439e
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-07-16 16:19:10 +0300

    Lab exercises

---
+++ Software Technologies/01Javascript Basic syntaxis/Lab/03SumTwoNumbers/SumTwoNumbers.js
@@ -0,0 +1,6 @@
+function sum(numbers) {
+    let firstNumber = Number(numbers[0]);
+    let secondNumber = Number(numbers[1]);
+    let result = Number(firstNumber + secondNumber);
+    console.log((result));
+}
