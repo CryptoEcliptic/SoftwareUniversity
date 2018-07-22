2b58029aaec1c9c15eaa22032bcf3c1503115af7
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-07-22 08:42:19 +0300

    Exercises

---
+++ Software Technologies/01Javascript Basic syntaxis/Exercises/01MultiplyNumberByTwo/01MultiplyNumberBy2.js
@@ -0,0 +1,5 @@
+function multiplyNumbers(numbers) {
+    let num = Number(numbers[0])
+    return num * 2
+
+}
---
+++ Software Technologies/01Javascript Basic syntaxis/Exercises/02MultiplyTwoNumbers/02MultiplyTwoNumbers.js
@@ -0,0 +1,4 @@
+function multiplyTwoNumbers(nums){
+    let multiply = Number(nums[0] * nums[1]);
+    return multiply;
+}
