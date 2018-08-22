3434fc007b6d0e07e8147e7eeccd9c6e55188eba
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-08-22 13:55:38 +0300

    Exercises

---
+++ Software Technologies/07Java Syntaxis exercises/p03ReverseCharacters.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class p03ReverseCharacters {
+
+    public  static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        String n1 = scan.nextLine();
+        String n2 = scan.nextLine();
+        String n3 = scan.nextLine();
+
+        System.out.print(n3+n2+n1);}
+
+
+}
