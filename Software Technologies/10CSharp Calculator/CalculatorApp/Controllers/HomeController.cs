c5e665804a4bab0122f3f07c42e5ba97df20ed96
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-09-09 12:43:03 +0300

    Calculator written in CSharp using ASP.Net

---
+++ Software Technologies/10CSharp Calculator/CalculatorApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        public IActionResult Calculate(Calculator calculator)
+        {
+            if (calculator.RightOperand == 0)
+            {
+                return RedirectToAction("Index", calculator);
+            }
+            calculator.CalculateResult();
+
+            return RedirectToAction("Index", calculator);
+
+        }
+
+    }
+}
