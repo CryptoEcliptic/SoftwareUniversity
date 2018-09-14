4c41cf9b7201b6b053b45ff46c76ce83859b9787
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-09-14 15:56:16 +0300

    Stacks and queues Lab

---
+++ C# Advanced/01 Stacks and Queues/Lab/01ReverseStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
