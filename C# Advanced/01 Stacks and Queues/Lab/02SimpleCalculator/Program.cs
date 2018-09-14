3dc654801920362878d21d59c83de03f53c86533
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-09-14 15:59:13 +0300

    Stacks and queues Lab

---
+++ C# Advanced/01 Stacks and Queues/Lab/02SimpleCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(' ')
+                .Reverse()
+                .ToArray();
+
+            Stack<string> stack = new Stack<string>();
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+
+            while (stack.Count > 1)
+            {
+                int leftoperand = int.Parse(stack.Pop());
+                string operation = stack.Pop();
+                int rightoperand = int.Parse(stack.Pop());
+                switch (operation)
+                {
+                    case "+":
+                        stack.Push((leftoperand + rightoperand).ToString());
+                        break;
+
+                    case "-":
+                        stack.Push((leftoperand - rightoperand).ToString());
+                        break;
+                }
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
