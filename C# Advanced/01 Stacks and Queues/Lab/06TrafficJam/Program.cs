51b6344d250837706defad2e2a5a666c224c0242
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-09-19 15:49:03 +0300

    Lab exercise

---
+++ C# Advanced/01 Stacks and Queues/Lab/06TrafficJam/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPassings = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+            Queue<string> waitingCars = new Queue<string>();
+            int counter = 0;
+            while (input != "end")
+            {
+                if (input != "green")
+                {
+                    waitingCars.Enqueue(input);
+                }
+                else if (input == "green")
+                {
+                    int carsCanPass = Math.Min(numberPassings, waitingCars.Count);
+
+                    for (int i = 1; i <= carsCanPass ; i++)
+                    {
+                        counter++;
+                        Console.WriteLine($"{waitingCars.Dequeue()} passed!");
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+
+        }
+    }
+}
