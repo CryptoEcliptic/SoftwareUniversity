3170f3a5ff93431fafa5830f224c8c1fbd68c060
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-09-19 15:47:50 +0300

    Lab exercises

---
+++ C# Advanced/01 Stacks and Queues/Lab/05MatchingBrackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> indexesOpenBrackets = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    indexesOpenBrackets.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int openBracketIndex = indexesOpenBrackets.Pop();
+                    int length = i - openBracketIndex + 1;
+                    Console.WriteLine(input.Substring(openBracketIndex, length));
+                }
+            }
+
+        }
+    }
+}
