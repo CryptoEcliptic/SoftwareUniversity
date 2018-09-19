40d876ee002d4af544a5b36504e46b788078fbd4
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-09-19 15:50:42 +0300

    Exercise

---
+++ C# Advanced/01 Stacks and Queues/Exercise/01StackAndQueuesExercises/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01StackAndQueuesExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split();
+
+            Stack<string> collectionStack = new Stack<string>(numbers);
+            int length = collectionStack.Count;
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(collectionStack.Pop() + " ");
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
---
+++ C# Advanced/01 Stacks and Queues/Exercise/02BasicStackOperations/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] commands = Console.ReadLine().Split()
+                 .Select(int.Parse)
+                 .ToArray();
+
+            int pushElements = commands[0];
+            int popElements = commands[1];
+            int lookFor = commands[2];
+
+            int[] numbers = Console.ReadLine().Split()
+                 .Select(int.Parse)
+                 .ToArray();
+            Stack<int> stack = new Stack<int>();
+
+            if (numbers.Count() >= pushElements)
+            {
+                for (int i = 0; i < pushElements; i++)
+                {
+                    stack.Push(numbers[i]);
+                }
+            }
+            if (stack.Count >= popElements)
+            {
+                for (int i = 0; i < popElements; i++)
+                {
+                    stack.Pop();
+                }
+            }
+            if (stack.Contains(lookFor))
+            {
+                Console.WriteLine("true");
+            }
+            else if(!stack.Contains(lookFor) && stack.Count > 0)
+            {
+                Console.WriteLine(stack.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
