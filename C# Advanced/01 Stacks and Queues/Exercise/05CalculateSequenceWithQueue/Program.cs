37df15be360119a2219700e2fc2fa04e6507c4a9
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-09-20 16:57:47 +0300

    Exercises

---
+++ C# Advanced/01 Stacks and Queues/Exercise/05CalculateSequenceWithQueue/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05CalculateSequenceWithQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long startNumber = long.Parse(Console.ReadLine());
+
+            Queue<long> collectionNumbers = new Queue<long>();
+            Queue<long> working = new Queue<long>();
+
+            collectionNumbers.Enqueue(startNumber);
+            working.Enqueue(startNumber);
+            long s1 = startNumber;
+
+            for (int i = 0; i < 50; i++)
+            {
+                long s2 = s1 + 1;
+                collectionNumbers.Enqueue(s2);
+                working.Enqueue(s2);
+
+                long s3 = 2 * s1 + 1;
+                collectionNumbers.Enqueue(s3);
+                working.Enqueue(s3);
+
+                long s4 = s1 + 2;
+                collectionNumbers.Enqueue(s4);
+                working.Enqueue(s4);
+
+                working.Dequeue();
+                s1 = working.Peek();
+            }
+
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write(collectionNumbers.Dequeue() + " ");
+            }
+
+
+        }
+    }
+}
---
+++ C# Advanced/01 Stacks and Queues/Exercise/06TruckTour/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int turns = int.Parse(Console.ReadLine());
+            Queue<long[]> data = new Queue<long[]>();
+
+            for (int pumpNumber = 0; pumpNumber < turns; pumpNumber++)
+            {
+                long[] petrolPumps = Console.ReadLine()
+                    .Split()
+                    .Select(long.Parse)
+                    .ToArray();
+                data.Enqueue(petrolPumps);
+            }
+            
+            for (int currentStart = 0; currentStart < turns - 1; currentStart++)
+            {
+                long fuel = 0;
+                bool isTrue = true;
+                for (int j = 0; j < turns; j++)
+                {
+                    long[] currnetPump = data.Dequeue();
+                    long currentFuel = currnetPump[0];
+                    long distance = currnetPump[1];
+                    data.Enqueue(currnetPump);
+
+                    fuel += currentFuel - distance;
+                    if (fuel < 0)
+                    {
+                        currentStart += j;
+                        isTrue = false;
+                        break;
+                    }
+                }
+                if (isTrue)
+                {
+                    Console.WriteLine(currentStart);
+                    Environment.Exit(0);
+                }
+                
+            }
+            
+        }
+    }
+}
