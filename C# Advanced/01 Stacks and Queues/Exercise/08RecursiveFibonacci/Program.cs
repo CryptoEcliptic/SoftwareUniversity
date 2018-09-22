dc07220b4be973c2a656058b81a0518e44eb74bf
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-09-22 13:47:10 +0300

    Exercises

---
+++ C# Advanced/01 Stacks and Queues/Exercise/08RecursiveFibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _08RecursiveFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Fibonacci(0, 1, 1, number);
+        }
+
+        public static void Fibonacci(long a, long b, long counter, int number)
+        {
+            
+            if (counter <= number)
+            {
+                Fibonacci(b, a + b, counter + 1, number);
+            }
+            long result = a;
+            Console.WriteLine(result);
+            Environment.Exit(0);
+        }
+    }
+}
