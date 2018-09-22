1acf35379647ed9123d6e2a7373a06d352a6b38f
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-09-22 13:48:03 +0300

    Exercises

---
+++ C# Advanced/01 Stacks and Queues/Exercise/09Stack Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09Stack_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            long a = 0;
+            long b = 1;
+            Stack<long> stack = new Stack<long>();
+            stack.Push(a);
+            stack.Push(b);
+
+            for (int i = 1; i < number; i++)
+            {
+                long first = stack.Pop();
+                long second = stack.Pop();
+
+                stack.Push(first);
+                stack.Push(first + second);
+            }
+
+            Console.WriteLine(stack.Pop());
+           
+        }
+    }
+}
