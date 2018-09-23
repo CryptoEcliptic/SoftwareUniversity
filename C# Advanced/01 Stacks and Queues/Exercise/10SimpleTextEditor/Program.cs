e9f4a9801cf1c97da5b9fe9c5d7880346b7d201c
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-09-23 08:24:56 +0300

    Exercises

---
+++ C# Advanced/01 Stacks and Queues/Exercise/10SimpleTextEditor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _10SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputLines = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+            Stack<string> collections = new Stack<string>();
+            collections.Push("");
+
+            for (int i = 0; i < inputLines; i++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                int command = int.Parse(input[0]);
+
+                switch (command)
+                {
+                    case 1:
+                        collections.Push(sb.ToString());
+                        string text = input[1];
+                        sb.Append(text);
+                        break;
+
+                    case 2:
+                        collections.Push(sb.ToString());
+                        int lengthRemove = int.Parse(input[1]);
+                        sb.Remove(sb.Length - lengthRemove, lengthRemove);
+                        break;
+
+                    case 3:
+                        int indexToPrint = int.Parse(input[1]);
+                        Console.WriteLine(sb[indexToPrint - 1]);
+                        break;
+
+                    case 4:
+                        sb.Clear();
+                        sb.Append(collections.Pop());
+                        break;
+                }
+            }
+        }
+    }
+}
