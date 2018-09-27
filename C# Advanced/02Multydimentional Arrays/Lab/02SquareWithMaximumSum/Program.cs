df92ca9af4893b0faf6abb5e1c01fc510be7d82a
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-09-27 16:27:58 +0300

    Lab exercises

---
+++ C# Advanced/02Multydimentional Arrays/Lab/02SquareWithMaximumSum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _02SquareWithMaximumSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[size[0], size[1]];
+
+            for (int i = 0; i < size[0]; i++)
+            {
+                int[] lineInput = Console.ReadLine()
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int j = 0; j < size[1]; j++)
+                {
+                    matrix[i, j] = lineInput[j];
+                }
+            }
+            int biggestSum = 0;
+            int rowIndex = 0; int colindex = 0;
+
+            for (int rows = 0; rows < matrix.GetLength(0) - 1; rows++)
+            {
+                for (int columns = 0; columns < matrix.GetLength(1) - 1; columns++)
+                {
+                    int tempSum = matrix[rows, columns] + matrix[rows, columns + 1] +
+                        matrix[rows + 1, columns] + matrix[rows + 1, columns + 1];
+
+                    if (tempSum > biggestSum)
+                    {
+                        biggestSum = tempSum;
+                        rowIndex = rows;
+                        colindex = columns;
+                    }
+                }
+            }
+            Console.WriteLine(matrix[rowIndex, colindex] + " " + matrix[rowIndex, colindex + 1]);
+            Console.WriteLine(matrix[rowIndex + 1, colindex] + " " + matrix[rowIndex + 1, colindex + 1]);
+            Console.WriteLine(biggestSum);
+        }
+    }
+}
---
+++ C# Advanced/02Multydimentional Arrays/Lab/03GroupNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _03GroupNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] size = new int[3];
+            foreach (var number in numbers)
+            {
+                size[Math.Abs(number % 3)]++;
+            }
+
+            int[][] jagged = new int[3][];
+            for (int i = 0; i < size.Length; i++)
+            {
+                jagged[i] = new int[size[i]];
+            }
+
+            int[] index = new int[3];
+            foreach (var number in numbers)
+            {
+                int reminder = Math.Abs(number % 3);
+                jagged[reminder][index[reminder]] = number;
+                index[reminder]++;
+            }
+
+            for (int row = 0; row < jagged.Length; row++)
+            {
+                for (int col = 0; col < jagged[row].Length; col++)
+                {
+                    Console.Write(jagged[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
