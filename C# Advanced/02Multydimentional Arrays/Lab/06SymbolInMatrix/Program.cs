90d00f2fdbdcd811b51671b910883e0ca917de96
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-09-28 13:59:23 +0300

    Lab exercises

---
+++ C# Advanced/02Multydimentional Arrays/Lab/06SymbolInMatrix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _06SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[matrixSize, matrixSize];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string inputChars = Console.ReadLine();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = inputChars[j];
+                }
+            }
+
+            char searchedSymbol = char.Parse(Console.ReadLine());
+            int row = -1;
+            int col = -1;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (searchedSymbol == matrix[i, j])
+                    {
+                        row = i;
+                        col = j;
+                        Console.WriteLine($"({row}, {col})");
+                        break;
+                    }
+                }
+            }
+            if (row < 0 && col < 0)
+            {
+                Console.WriteLine($"{searchedSymbol} does not occur in the matrix");
+            }
+        }
+    }
+}
