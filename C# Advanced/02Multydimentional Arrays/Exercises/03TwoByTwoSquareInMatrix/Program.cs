bfc1b1d023d8e82308aeccdb54ae086d15f10bee
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-09-30 11:01:57 +0300

    Exercises

---
+++ C# Advanced/02Multydimentional Arrays/Exercises/03TwoByTwoSquareInMatrix/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03TwoByTwoSquareInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputParams = Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int lines = inputParams[0];
+            int columns = inputParams[1];
+
+            char[,] matrix = new char[lines, columns];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                char[] input = Console.ReadLine()
+                   .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                   .Select(char.Parse)
+                   .ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+            FinnSquare(matrix);
+ 
+        }
+
+        private static void FinnSquare(char[,] matrix)
+        {
+            int counter = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] && 
+                        matrix[row, col] == matrix[row + 1, col] &&
+                        matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
