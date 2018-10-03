9ca5c47cd21c4e6cfd15e5c0bf3fe0668de08c2f
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-03 16:57:21 +0300

    Exercises

---
+++ C# Advanced/02Multydimentional Arrays/Exercises/07.1LegoBlocks/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._1LegoBlocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraysLines = int.Parse(Console.ReadLine());
+
+            int[][] firstArray = new int[arraysLines][];
+            int[][] secondArray = new int[arraysLines][];
+
+            FillFirstArray(arraysLines, firstArray);
+            FillSecondArray(arraysLines, secondArray);
+            ReverseSecondArray(secondArray);
+
+
+            int[][] matrix = new int[arraysLines][];
+            GatheredArrays(arraysLines, firstArray, secondArray, matrix);
+
+            int rowLenght = firstArray[0].Length + secondArray[0].Length;
+            bool isMatrix = true;
+            int elements = 0;
+            for (int i = 0; i < arraysLines; i++)
+            {
+                int currentLenght = firstArray[i].Length + secondArray[i].Length;
+                elements += matrix[i].Count();
+                if (currentLenght != rowLenght)
+                {
+                    isMatrix = false;
+                }
+            }
+            if (isMatrix)
+            {
+                PrintMatrix(matrix);
+            }
+            else if (isMatrix == false)
+            {
+                Console.WriteLine($"The total number of cells is: {elements}");
+            }
+        }
+
+        private static void PrintMatrix(int[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+
+                Console.WriteLine("[" + (String.Join(", ", matrix[i])) + "]");
+            }
+        }
+
+        private static void GatheredArrays(int arraysLines, int[][] firstArray, int[][] secondArray, int[][] matrix)
+        {
+            for (int i = 0; i < arraysLines; i++)
+            {
+                matrix[i] = new int[firstArray[i].Length + secondArray[i].Length];
+                int index = 0;
+
+                for (int j = 0; j < firstArray[i].Length; j++)
+                {
+                    matrix[i][index] = firstArray[i][j];
+                    index++;
+                }
+                for (int k = 0; k < secondArray[i].Length; k++)
+                {
+                    matrix[i][index] = secondArray[i][k];
+                    index++;
+                }
+
+            }
+        }
+
+        private static void ReverseSecondArray(int[][] secondArray)
+        {
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                for (int j = 0; j <= secondArray[i].Length / 2; j++)
+                {
+                    secondArray[i] = secondArray[i].Reverse().ToArray();
+                }
+            }
+        }
+
+        private static void FillSecondArray(int arraysLines, int[][] secondArray)
+        {
+            for (int i = 0; i < arraysLines; i++)
+            {
+                int[] inputNumbers = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                secondArray[i] = new int[inputNumbers.Length];
+                for (int j = 0; j < secondArray[i].Length; j++)
+                {
+                    secondArray[i][j] = inputNumbers[j];
+                }
+            }
+        }
+
+        private static void FillFirstArray(int arraysLines, int[][] firstArray)
+        {
+            for (int i = 0; i < arraysLines; i++)
+            {
+                int[] inputNumbers = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                firstArray[i] = new int[inputNumbers.Length];
+                for (int j = 0; j < firstArray[i].Length; j++)
+                {
+                    firstArray[i][j] = inputNumbers[j];
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/02Multydimentional Arrays/Exercises/07LegoBlocks/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07LegoBlocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsNum = int.Parse(Console.ReadLine());
+
+            int[][] leftBlock = new int[rowsNum][];
+            int[][] rightBlock = new int[rowsNum][];
+
+            for (int r = 0; r < rowsNum; r++)
+            {
+                leftBlock[r] = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+            }
+
+            for (int r = 0; r < rowsNum; r++)
+            {
+                rightBlock[r] = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).Reverse().ToArray();
+            }
+
+            int rowLenght = leftBlock[0].Length + rightBlock[0].Length;
+            int allElements = 0;
+            bool isMatch = true;
+
+            for (int r = 0; r < rowsNum; r++)
+            {
+                int currentLenght = leftBlock[r].Length + rightBlock[r].Length;
+                if (currentLenght != rowLenght)
+                {
+                    isMatch = false;
+                }
+
+                allElements += currentLenght;
+            }
+
+            if (isMatch)
+            {
+                int[] resultRow = new int[rowLenght];
+                for (int r = 0; r < rowsNum; r++)
+                {
+                    resultRow = leftBlock[r].Concat(rightBlock[r]).ToArray();
+                    Console.WriteLine($"[{string.Join(", ", resultRow)}]");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The total number of cells is: {allElements}");
+            }
+        }
+    }
+ }
+
