aceb6160ae3a3970386bf98a8e5bd3993c11e771
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-06 18:53:43 +0300

    Exercises

---
+++ C# Advanced/02Multydimentional Arrays/Exercises/11ParkingSystem/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11ParkingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] size = Console.ReadLine().Split();
+            int row = int.Parse(size[0]);
+            int col = int.Parse(size[1]);
+
+            int[][] parking = new int[row][];
+
+            string input;
+            while ((input = Console.ReadLine()) != "stop")
+            {
+                int entryRow = int.Parse(input.Split()[0]);
+                int parkRow = int.Parse(input.Split()[1]);
+                int parkCol = int.Parse(input.Split()[2]);
+
+                if (parking[parkRow] == null)
+                {
+                    parking[parkRow] = new int[col];
+                }
+
+                if (parking[parkRow].Skip(1).All(x => x == 1))
+                {
+                    Console.WriteLine($"Row {parkRow} full");
+                    continue;
+                }
+
+                if (parking[parkRow][parkCol] == 1)
+                {
+                    parkCol = GetNearestFreeCol(parking, parkRow, parkCol);
+                }
+
+                parking[parkRow][parkCol] = 1;
+
+                int distance = Math.Abs(entryRow - parkRow) + parkCol + 1;
+                Console.WriteLine(distance);
+            }
+        }
+        private static int GetNearestFreeCol(int[][] parking, int parkRow, int parkCol)
+        {
+            for (int i = 1; i < parking[parkRow].Length; i++)
+            {
+                if (parkCol > i && parking[parkRow][parkCol - i] != 1)
+                    return parkCol - i;
+                if (parkCol + i < parking[parkRow].Length && parking[parkRow][parkCol + i] != 1)
+                    return parkCol + i;
+            }
+            return -1; // It's just needed. Will never be returned
+        }
+    }
+}
---
+++ C# Advanced/02Multydimentional Arrays/Exercises/12StringMatrixRotation/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12StringMatrixRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rotate = Console.ReadLine();
+            string[] rotationtextList = rotate.Split(new char[] { '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+            int degrees = int.Parse(rotationtextList[1]);
+
+            string text = Console.ReadLine();
+            List<string> textList = new List<string>();
+            int longestWord = 0;
+
+            while (text != "END")
+            {
+                textList.Add(text);
+                if (longestWord < text.Length)
+                {
+                    longestWord = text.Length;
+                }
+                text = Console.ReadLine();
+            }
+
+            for (int i = 0; i < textList.Count; i++)
+            {
+                if (textList[i].Length < longestWord)
+                {
+                    textList[i] = textList[i] + new string(' ', longestWord - textList[i].Length);
+                }
+            }
+
+            if (degrees == 180 || degrees % 360 == 180)
+            {
+                Rotate180(textList);
+            }
+
+            char[,] rotate90 = new char[longestWord, textList.Count];
+            CreateRotatedMatrix(textList, longestWord, rotate90);
+
+            if (degrees == 270 || degrees % 360 == 270)
+            {
+                Rotate270(rotate90);
+            }
+
+            if (degrees == 90 || degrees % 360 == 90)
+            {
+                Rotate90(rotate90);
+            }
+
+            if (degrees == 0 || degrees % 360 == 0)
+            {
+                foreach (var rotate360 in textList)
+                {
+                    Console.WriteLine(rotate360);
+                }
+
+            }
+        }
+        private static void Rotate90(char[,] rotate90)
+        {
+            for (int i = 0; i < rotate90.GetLength(0); i++)
+            {
+                for (int j = rotate90.GetLength(1) - 1; j >= 0; j--)
+                {
+                    Console.Write(rotate90[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void Rotate270(char[,] rotate90)
+        {
+            for (int i = rotate90.GetLength(0) - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < rotate90.GetLength(1); j++)
+                {
+                    Console.Write(rotate90[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void CreateRotatedMatrix(List<string> textList, int longestWord, char[,] rotate90)
+        {
+            for (int row = 0; row < longestWord; row++)
+            {
+                for (int col = 0; col < textList.Count; col++)
+                {
+                    rotate90[row, col] = textList[col][row];
+                }
+            }
+        }
+
+        private static void Rotate180(List<string> textList)
+        {
+            for (int i = textList.Count - 1; i >= 0; i--)
+            {
+                for (int j = textList[i].Length - 1; j >= 0; j--)
+                {
+                    Console.Write(textList[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
