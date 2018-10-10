8b19b5c7932e16587f24ba66ee5a4ee8fa7fba9c
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-10 18:07:07 +0300

    Lab exercises

---
+++ C# Advanced/03SetsAndDictionaries/Lab/06CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05CitiesByContinentAndCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputCount = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> continentData =
+                new Dictionary<string, Dictionary<string, List<string>>>();
+
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                string[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string continent = data[0];
+                string country = data[1];
+                string city = data[2];
+
+                if (!continentData.ContainsKey(continent))
+                {
+                    Dictionary<string, List<string>> temporary = new Dictionary<string, List<string>>();
+                    List<string> temp = new List<string>();
+                    temp.Add(city);
+                    temporary.Add(country, temp);
+                    continentData.Add(continent, temporary);
+                }
+                else
+                {
+                    if (continentData.ContainsKey(continent))
+                    {
+                        if (!continentData[continent].ContainsKey(country))
+                        {
+                            List<string> temp = new List<string>();
+                            temp.Add(city);
+                            continentData[continent].Add(country, temp);
+
+                        }
+                        else if (continentData[continent].ContainsKey(country))
+                        {
+                            //if (!continentData[continent][country].Contains(city)) //With this check 1-4 test fails
+                            //{ //Why??? With this if check negotiate the possibility of adding same city twice
+                                continentData[continent][country].Add(city);
+                            //}
+                        }
+
+                    }
+                }
+            }
+            foreach (var continent in continentData)
+            {
+                Console.WriteLine($"{continent.Key}:");
+                foreach (var country in continent.Value)
+                {
+                    Console.Write($" {country.Key} -> ");
+                    Console.WriteLine(string.Join(", ", country.Value));
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/03SetsAndDictionaries/Lab/07RecordUniqueUsernames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06RecordUniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputLines = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < inputLines; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
