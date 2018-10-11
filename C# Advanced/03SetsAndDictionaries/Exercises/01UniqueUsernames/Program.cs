ee2aa92513c0a84295f18b946be8d1fe261ec2d0
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-11 15:58:25 +0300

    Exercises

---
+++ C# Advanced/03SetsAndDictionaries/Exercises/01UniqueUsernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfEntries = int.Parse(Console.ReadLine());
+            HashSet<string> usernamesDb = new HashSet<string>();
+
+            for (int i = 0; i < numberOfEntries; i++)
+            {
+                string username = Console.ReadLine();
+                usernamesDb.Add(username);
+            }
+
+            foreach (var name in usernamesDb)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
