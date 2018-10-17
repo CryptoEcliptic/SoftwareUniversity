4b15aec4209259b0278c60c4b95c5a233bc3c914
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-17 17:10:25 +0300

    Exercises

---
+++ C# Advanced/03SetsAndDictionaries/Exercises/03PeriodicTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            SortedSet<string> table = new SortedSet<string>();
+            for (int i = 0; i < inputNumber; i++)
+            {
+                string[] element = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < element.Length; j++)
+                {
+                    if (!table.Contains(element[j]))
+                    {
+                        table.Add(element[j]);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", table));
+        }
+    }
+}
