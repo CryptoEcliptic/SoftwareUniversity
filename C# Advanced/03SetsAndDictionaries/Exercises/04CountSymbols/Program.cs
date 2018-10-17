43143615f451d21573bef8062700b3bc3f582fb6
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-17 17:11:42 +0300

    Exercises

---
+++ C# Advanced/03SetsAndDictionaries/Exercises/04CountSymbols/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentSymbol = input[i];
+                if (!symbols.ContainsKey(currentSymbol))
+                {
+                    symbols.Add(currentSymbol, 1);
+                }
+                else
+                {
+                    symbols[currentSymbol]++;
+                }
+            }
+
+            foreach (var letter in symbols)
+            {
+                Console.WriteLine($"{letter.Key}: {letter.Value} time/s");
+            }
+        }
+    }
+}
