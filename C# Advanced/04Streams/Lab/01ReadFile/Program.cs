3bac9ebc4b95b6fa0dfce5389dfb64b856776645
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-18 17:23:33 +0300

    Lab exercises

---
+++ C# Advanced/04Streams/Lab/01ReadFile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace _01ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new StreamReader("C:\\Users\\Name\\source\\repos\\04StreamsLab\\01ReadFile\\Program.cs"))
+            {
+                int lineNumber = 1;
+                string line = stream.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine($"Line {lineNumber}: {line}");
+                    line = stream.ReadLine();
+                    lineNumber++;
+                }
+            }
+        }
+    }
+}
