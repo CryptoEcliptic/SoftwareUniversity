cd411fc8a0bd0ac7cedb251778f3e30fe43cf430
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-18 17:25:50 +0300

    Lab exercises

---
+++ C# Advanced/04Streams/Lab/03WritingTextToFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace _03WritingTextToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Кирилица";
+            FileStream stream = null;
+
+            try
+            {
+                stream = new FileStream("log.txt", FileMode.Create);
+                var bytes = System.Text.Encoding.UTF8.GetBytes(text);
+                stream.Write(bytes, 0, bytes.Length);
+            }
+            finally
+            {
+                stream.Close();
+            }
+        }
+    }
+}
---
+++ C# Advanced/04Streams/Lab/04StreamsLab/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _01ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new StreamReader("Program.cs"))
+            {
+                int lineNumber = 1;
+                string line = stream.ReadLine();
+                while (line != null)
+                {
+
+                    Console.WriteLine($"Line {lineNumber}: {line}");
+                    line = stream.ReadLine();
+                    lineNumber++;
+                }
+            }
+        }
+    }
+}
