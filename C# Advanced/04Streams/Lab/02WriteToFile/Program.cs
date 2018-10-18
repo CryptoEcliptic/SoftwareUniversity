3e11a9f835a91340f75cf39689df4bf05e18673b
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-18 17:24:50 +0300

    Lab exercises

---
+++ C# Advanced/04Streams/Lab/02WriteToFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+
+namespace _02WriteToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var readStream = new StreamReader("C:\\Users\\Name\\source\\repos\\04StreamsLab\\01ReadFile\\Program.cs"))
+            {
+                using (var writeStream = new StreamWriter("reversed.txt"))
+                {
+                    string line = readStream.ReadLine();
+                    while (line != null)
+                    {
+
+                        for (int i = line.Length - 1; i >= 0; i--)
+                        {
+                            writeStream.Write(line[i]);
+                        }
+                        writeStream.WriteLine();
+
+
+                        line = readStream.ReadLine();
+
+                    }
+                }
+               
+            }
+        }
+    }
+}
