7d065c98cd54aec6a3d7809b12782c3a8455aaf8
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-20 20:10:08 +0300

    Exercises

---
+++ C# Advanced/04Streams/Exercises/01OddLines/OddLines.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace STREAMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lineNumber = 0;
+            using (StreamReader reader = new StreamReader(@"../../../../resources/text.txt"))
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (lineNumber % 2 == 1)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    lineNumber++;
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
