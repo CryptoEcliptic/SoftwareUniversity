543f64842e6ae252ad5a567dedd2b50d71d64b7c
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-20 20:11:44 +0300

    Exercises

---
+++ C# Advanced/04Streams/Exercises/02LineNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _02LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lineNumber = 1;
+            using (StreamReader reader = new StreamReader(@"..\..\..\..\resources\text.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter(@"..\..\..\output.txt"))
+                {
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        writer.WriteLine($"Line {lineNumber}: {line}");
+                        lineNumber++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
