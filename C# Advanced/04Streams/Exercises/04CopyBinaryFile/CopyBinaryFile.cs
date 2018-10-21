69f010212d8a3e78ff7de27d9e3140eb0ae7cc91
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-21 16:47:59 +0300

    Exercises

---
+++ C# Advanced/04Streams/Exercises/04CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace _04CopyBinaryFile
+{
+    class CopyBinaryFile
+    {
+        const string imagePath = @"..\..\..\..\Resources\copyMe.png";
+        const string destinationPath = @"..\..\..\result.png";
+        static void Main(string[] args)
+        {
+
+            using (var readFile = new FileStream(imagePath, FileMode.Open))
+            {
+                using (var createCopy = new FileStream(destinationPath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[readFile.Length];
+                    while (true)
+                    {
+                        int readBytes = readFile.Read(buffer, 0, buffer.Length);
+                        if (readBytes == 0)
+                        {
+                            break;
+                        }
+                        createCopy.Write(buffer, 0, readBytes);
+                    }
+                }
+            }
+        }
+    }
+}
