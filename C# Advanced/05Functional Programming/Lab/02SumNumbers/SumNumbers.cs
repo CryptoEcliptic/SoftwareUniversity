c300c037e56525b6f09b5bd45ca5f3bd358ab8fc
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-25 16:41:35 +0300

    Lab exercises

---
+++ C# Advanced/05Functional Programming/Lab/02SumNumbers/SumNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _02SumNumbers
+{
+    class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(", ")
+                .Select(int.Parse)
+                .ToList();
+
+            Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
