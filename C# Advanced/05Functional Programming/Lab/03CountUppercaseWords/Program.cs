9be58e691e19ec59dd69bc4d12e8df19811846fa
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-25 16:42:25 +0300

    Lab exercises

---
+++ C# Advanced/05Functional Programming/Lab/03CountUppercaseWords/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _03CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sentance = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => Char.IsUpper(x[0]))
+                .ToArray();
+
+            foreach (var word in sentance)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
