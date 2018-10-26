0a1023c32b23e65e6565bce30d2e5968e2c9d18f
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-26 16:16:02 +0300

    Lab exercises

---
+++ C# Advanced/05Functional Programming/Lab/04AddVAT/AddVAT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _04AddVAT
+{
+    class AddVAT
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+               .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(x => double.Parse(x, System.Globalization.CultureInfo.InvariantCulture))
+               .Select(x => x * 1.2)
+               .ToArray();
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+        }
+    }
+}
