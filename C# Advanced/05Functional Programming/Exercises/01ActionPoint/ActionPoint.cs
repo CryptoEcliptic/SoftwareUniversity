2d2052f6d70dad6b16d19f0dc43ee739cb86d2f8
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-28 06:38:41 +0200

    Exercises

---
+++ C# Advanced/05Functional Programming/Exercises/01ActionPoint/ActionPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _01ActionPolong
+{
+    class ActionPolong
+    {
+        static void Main(string[] args)
+        {
+            Action<string> prlong = x => Console.WriteLine(x);
+
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(prlong);
+        }
+    }
+}
