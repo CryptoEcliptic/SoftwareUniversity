e627eb9a59d65c196865ffd771f58666bcfc131e
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-28 06:40:28 +0200

    Exercises

---
+++ C# Advanced/05Functional Programming/Exercises/02NightsOfHonor/NightsOfHonor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _02NightsOfHonor
+{
+    class NightsOfHonor
+    {
+        static void Main(string[] args)
+        {
+            Action<string> prlong = x => Console.WriteLine($"Sir {x}");
+
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(prlong);
+        }
+    }
+}
