cbbf063209e2cb4877b9069197c59c7605173adb
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-31 20:36:50 +0200

    Exercises

---
+++ C# Advanced/05Functional Programming/Exercises/05AppliedArithmetics/AppliedArithmetics.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _05AppliedArithmetics
+{
+    class AppliedArithmetics
+    {
+        static void Main(string[] args)
+        {
+            double[] inputNumbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        for (long i = 0; i < inputNumbers.Length; i++)
+                        {
+                            Func<double, double> adding = x => x + 1;
+                            inputNumbers[i] = adding(inputNumbers[i]);
+                        }
+                        break;
+
+                    case "multiply":
+                        for (long i = 0; i < inputNumbers.Length; i++)
+                        {
+                            Func<double, double> multyplier = x => x * 2;
+                            inputNumbers[i] = multyplier(inputNumbers[i]);
+                        }
+                        break;
+
+                    case "subtract":
+                        for (long i = 0; i < inputNumbers.Length; i++)
+                        {
+                            Func<double, double> subtraction = x => x - 1;
+                            inputNumbers[i] = subtraction(inputNumbers[i]);
+                        }
+                        break;
+
+                    case "print":
+                        Console.WriteLine(string.Join(" ", inputNumbers));
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
