6b1f355f6d837333b2eb5d325a18d7b65b916785
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-31 20:35:55 +0200

    Exercises

---
+++ C# Advanced/05Functional Programming/Exercises/04FindEvensOrOdds/EvensOrOdds.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04FindEvensOrOdds
+{
+    class EvensOrOdds
+    {
+        static void Main(string[] args)
+        {
+            long[] range = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Select(long.Parse)
+                 .ToArray();
+            long start = range[0];
+            long end = range[1];
+
+            string condition = Console.ReadLine();
+
+            List<long> numbers = new List<long>();
+            FillList(start, end, numbers);
+
+            Predicate<long> even = x => { return x % 2 == 0; };
+            Predicate<long> odd = x => { return x % 2 != 0; };
+
+            if (condition == "even")
+            {
+                numbers = numbers.FindAll(even);
+            }
+            else
+            {
+                numbers = numbers.FindAll(odd);
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+
+        private static void FillList(long start, long end, List<long> numbers)
+        {
+            for (long i = start; i <= end; i++)
+            {
+                numbers.Add(i);
+            }
+        }
+    }
+}
