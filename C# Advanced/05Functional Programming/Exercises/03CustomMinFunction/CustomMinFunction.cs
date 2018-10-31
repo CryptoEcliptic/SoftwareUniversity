4eabeeda9c539385876f8c6955072465b8711e7d
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-10-31 20:34:41 +0200

    Exercises

---
+++ C# Advanced/05Functional Programming/Exercises/03CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _03CustomMinFunction
+{
+    class CustomMinFunction
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Select(double.Parse)
+                 .ToArray();
+            Func<double[], double> defineMinimalNumber = FindMinimalNumber;
+            var minimalNumber = defineMinimalNumber(numbers);
+
+            Console.WriteLine(minimalNumber);
+        }
+
+        private static double FindMinimalNumber(double[] numbers)
+        {
+            double minNumber = double.MaxValue;
+            foreach (var number in numbers)
+            {
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
+            }
+            return minNumber;
+        }
+    }
+}
