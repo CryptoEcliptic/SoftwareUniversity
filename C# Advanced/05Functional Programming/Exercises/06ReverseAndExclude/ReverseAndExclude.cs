b66d31d1ff768581e723f09ece43ff87bba10011
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-11-01 16:34:18 +0200

    Exercises

---
+++ C# Advanced/05Functional Programming/Exercises/06ReverseAndExclude/ReverseAndExclude.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06ReverseAndExclude
+{
+    class ReverseAndExclude
+    {
+        static void Main(string[] args)
+        {
+            var inputNumbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+
+            int divisionNumber = int.Parse(Console.ReadLine());
+
+            Func<int, bool> divisibleByNumber = x => (x % divisionNumber == 0);
+
+            List<int> result = new List<int>();
+            for (int i = 0; i < inputNumbers.Count; i++)
+            {
+                int currentNumber = inputNumbers[i];
+                if (!divisibleByNumber(currentNumber))
+                {
+                    result.Add(currentNumber);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
