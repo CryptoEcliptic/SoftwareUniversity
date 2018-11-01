0eddf30aa088af4d208a6921756d2935fdde47c4
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-11-01 16:35:38 +0200

    Exercises

---
+++ C# Advanced/05Functional Programming/Exercises/07PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _07PredicateForNames
+{
+    class PredicateForNames
+    {
+        static void Main(string[] args)
+        {
+            long lengthName = long.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.Length <= lengthName)
+                .ToArray();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
