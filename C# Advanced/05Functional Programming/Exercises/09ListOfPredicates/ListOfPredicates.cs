47a592b14124817a94165ec12f3fdd6512dfe963
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-11-02 15:41:32 +0200

    Exercises

---
+++ C# Advanced/05Functional Programming/Exercises/09ListOfPredicates/ListOfPredicates.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09ListOfPredicates
+{
+    class ListOfPredicates
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+
+            int[] dividers = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .Distinct()
+                 .ToArray();
+
+            List<int> divisibleNumbers = new List<int>();
+            for (int i = 1; i <= range; i++)
+            {
+                int currentNumber = i;
+                if (FindDivisibleNumbers(dividers, currentNumber))
+                {
+                    divisibleNumbers.Add(currentNumber);
+                }
+            }
+            Console.WriteLine(string.Join(" ", divisibleNumbers));
+        }
+        private static bool FindDivisibleNumbers(int[] dividers, int currentNum)
+        {
+            bool isDivisible = true;
+
+            foreach (var div in dividers)
+            {
+                if (currentNum % div != 0)
+                {
+                    isDivisible = false;
+                }
+            }
+            return isDivisible;
+        }
+    }
+}
