2e43862846e835dd3167fe303eb434b1d50eed08
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-11-09 15:09:54 +0200

    Exercises

---
+++ C# Advanced/05Functional Programming/Exercises/13TriFunction/TryFunction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _13TriFunction
+{
+    class TryFunction
+    {
+        static void Main(string[] args)
+        {
+         
+            int refNumber = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Func<int, int, bool> functionLettersValue = (int name, int value) => name >= value;
+
+            foreach (var name in names)
+            {
+                char[] nameAsChar = name.ToCharArray();
+
+                int sumLetters = 0;
+                foreach (var letter in nameAsChar)
+                {
+                    sumLetters += letter;
+                }
+                if (functionLettersValue(sumLetters, refNumber))
+                {
+                    Console.WriteLine(name);
+                    break;
+                }
+            }
+
+        }
+    }
+}
