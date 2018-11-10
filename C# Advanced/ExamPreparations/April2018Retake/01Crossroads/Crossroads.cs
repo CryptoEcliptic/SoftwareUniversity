f79de60364fabccc6e44f536dd062110236af6b4
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-11-10 18:43:14 +0200

    Exam preparation

---
+++ C# Advanced/ExamPreparations/April2018Retake/01Crossroads/Crossroads.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _01Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLigntTime = int.Parse(Console.ReadLine());
+            int window = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+
+            string inputCar = Console.ReadLine();
+            bool hasCarAccident = false;
+            int count = 0;
+
+            while (inputCar != "END")
+            {
+                if (inputCar == "green")
+                {
+                    int currentGreen = greenLigntTime;
+                    int currentWindow = window;
+                    while (true)
+                    {
+                        if (cars.Count <= 0 || currentGreen <= 0)
+                        {
+                            break;
+                        }
+                        
+                        string car = cars.Peek();
+                        int greenAndWindow = currentGreen + currentWindow;
+
+                        if (car.Length <= currentGreen)
+                        {
+                            cars.Dequeue();
+                            count++;
+                            currentGreen -= car.Length;
+                        }
+
+                        else if (currentGreen > 0 && car.Length > currentGreen)
+                        {
+                            currentGreen -= car.Length; 
+                            greenAndWindow = currentWindow + currentGreen;
+                            if (greenAndWindow >= 0)
+                            {
+                                cars.Dequeue();
+                                count++;
+                            }
+                            else
+                            {
+                                int indexHit = Math.Abs(greenAndWindow);
+                                char hitCar = car[car.Length - indexHit];
+
+                                Console.WriteLine("A crash happened!");
+                                Console.WriteLine($"{car} was hit at {hitCar}.");
+                                hasCarAccident = true;
+                                return;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(inputCar);
+                }
+
+                inputCar = Console.ReadLine();
+            }
+
+            if (hasCarAccident == false)
+            {
+                Console.WriteLine("Everyone is safe.");
+                Console.WriteLine($"{count} total cars passed the crossroads.");
+            }
+
+        }
+    }
+}
---
+++ C# Advanced/ExamPreparations/April2018Retake/02ParkingFeud/02ParkngFeud.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02ParkingFeud
+{
+    class ParkingFeud
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimentions[0];
+            int cols = dimentions[1];
+
+            int samEntrance = int.Parse(Console.ReadLine());
+            bool succesfulParking = false;
+            CreateAndFillParkingArea(rows, cols);
+
+            while (succesfulParking == false)
+            {
+                string[] targetParkingLots = Console.ReadLine().Split(' ').ToArray();
+            }
+
+        }
+
+        private static void CreateAndFillParkingArea(int rows, int cols)
+        {
+            List<List<string>> parking = new List<List<string>>();
+            int number = 1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                List<string> currentRow = new List<string>();
+                char letter = 'A';
+                for (int j = 0; j < cols; j++)
+                {
+
+                    string name = letter + number.ToString();
+                    currentRow.Add(name);
+                    letter++;
+                }
+                parking.Add(currentRow);
+                number++;
+            }
+        }
+    }
+}
