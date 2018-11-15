c3e904d812472ee55483219f424396263fa19d32
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-11-15 20:52:35 +0200

    Exercises

---
+++ C# OOP Basics/01Defining Classes/Exercises/03OldestFamilyMember/Family.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03OldestFamilyMember
+{
+    public class Family
+    {
+        private List<Person> familyMembers;
+
+        public Family()
+        {
+            this.familyMembers = new List<Person>();
+        }
+
+        public void AddMembers(Person person)
+        {
+            if (person == null)
+            {
+                throw new Exception();
+            }
+            this.familyMembers.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.familyMembers.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+    }
+}
---
+++ C# OOP Basics/01Defining Classes/Exercises/03OldestFamilyMember/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _03OldestFamilyMember
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int membersCount = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < membersCount; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age);
+                family.AddMembers(person);
+            }
+
+            Person oldestOne = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestOne.Name} {oldestOne.Age}");
+
+            
+        }
+    }
+}
