533f71abe3fca0a390cd050c337aae47dc34951e
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-12-10 17:37:08 +0200

    Added problems 3 and 4

---
+++ C# OOP Advanced/01 SOLID/Lab/P03.Detail_Printer/Program.cs
@@ -0,0 +1,27 @@
+﻿using P03.Detail_Printer;
+using System;
+using System.Collections.Generic;
+
+namespace P03.DetailPrinter
+{
+    class Program
+    {
+        static void Main()
+        {
+            string inputEmployee = "Gosho";
+            string inputManager = "Pesho";
+            string[] docs = { "doc1", "doc2", "doc3" };
+
+            IEmployee first = new Employee(inputEmployee);
+            IEmployee second = new Manager(inputManager, docs);
+            List<IEmployee> employees = new List<IEmployee>();
+
+            employees.Add(first);
+            employees.Add(second);
+
+            DetailsPrinter details = new DetailsPrinter(employees);
+
+            details.PrintDetails();
+        }
+    }
+}
