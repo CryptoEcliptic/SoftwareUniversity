fd58297454f69c9b55a080591edfe9c54c6c3cc8
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-12-16 15:46:20 +0200

    Lab exercises

---
+++ C# OOP Advanced/02 Generics/Lab/01BoxOfIT/List.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public List<T> collection;
+   
+        public Box()
+        {
+            this.collection = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            collection.Add(element);
+        }
+
+        public T Remove()
+        {
+            var lastElement = collection.LastOrDefault();
+            this.collection.RemoveAt(this.collection.Count - 1);
+            return lastElement;
+        }
+
+        public int Count => this.collection.Count();
+
+    } 
+}
+
---
+++ C# OOP Advanced/02 Generics/Lab/02GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            return new T[length];
+        }
+    }
+}
