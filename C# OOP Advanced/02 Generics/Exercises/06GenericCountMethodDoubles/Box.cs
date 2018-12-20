848dd4e42ef54293968ecef9bf762c0586c0d341
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-12-20 10:28:47 +0200

    Exercises

---
+++ C# OOP Advanced/02 Generics/Exercises/06GenericCountMethodDoubles/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06GenericCountMethodDoubles
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        public Box() { }
+
+
+        public void CountGreaterElements(List<T> items, T itemToCompare)
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            if (itemToCompare.Equals(default(T)))
+            {
+                throw new InvalidOperationException();
+            }
+
+            int counter = 0;
+            foreach (var item in items)
+            {
+                if (item.CompareTo(itemToCompare) > 0) //IComparable for comparing T elements 
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ C# OOP Advanced/02 Generics/Exercises/06GenericCountMethodDoubles/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06GenericCountMethodDoubles
+{
+    class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<double> elements = new List<double>();
+
+            int numberInputs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberInputs; i++)
+            {
+               double element = double.Parse(Console.ReadLine());
+               elements.Add(element);
+            }
+
+            double comparedElement = double.Parse(Console.ReadLine());
+
+            Box<double> box = new Box<double>();
+            box.CountGreaterElements(elements, comparedElement);
+        }
+    }
+}
