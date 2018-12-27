2bccea1474c317d9e26827af64bfe27bb1da9868
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-12-27 18:58:52 +0200

    Exercises

---
+++ C# OOP Advanced/02 Generics/Exercises/12Threeuple/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _11Threeuple
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] inputOne = Console.ReadLine().Split();
+            Tuple<string, string, string> firstTupple = new Tuple<string, string, string>((inputOne[0] + " " + inputOne[1]), inputOne[2], inputOne[3]);
+
+            string[] inputTwo = Console.ReadLine().Split();
+            bool isDrunk = true;
+            if (inputTwo[2] != "drunk")
+            {
+                isDrunk = false;
+            }
+            Tuple<string, int, bool> secondTupple = new Tuple<string, int, bool>((inputTwo[0]), int.Parse(inputTwo[1]), isDrunk);
+
+            string[] inputThree = Console.ReadLine().Split();
+            Tuple<string, double, string> thirdTupple = new Tuple<string, double, string>(inputThree[0], double.Parse(inputThree[1]),
+                inputThree[2]);
+
+            Console.WriteLine(firstTupple.ToString());
+            Console.WriteLine(secondTupple.ToString());
+            Console.WriteLine(thirdTupple.ToString());
+        }
+    }
+}
---
+++ C# OOP Advanced/02 Generics/Exercises/12Threeuple/Tuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11Threeuple
+{
+    public class Tuple<T, U, P>
+    {
+        public Tuple(T itemOne, U ItemTwo, P itemThree)
+        {
+            this.ItemOne = itemOne;
+            this.ItemTwo = ItemTwo;
+            this.ItemThree = itemThree;
+        }
+
+        public T ItemOne { get; protected set; }
+        public U ItemTwo { get; protected set; }
+        public P ItemThree { get; protected set; }
+
+        public override string ToString()
+        {
+            return $"{this.ItemOne} -> {this.ItemTwo} -> {this.ItemThree}";
+        }
+    }
+}
