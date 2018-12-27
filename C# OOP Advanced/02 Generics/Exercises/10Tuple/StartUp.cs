3040b521d1abd0bb7d7d88e170a3d6a66adc0adf
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-12-27 18:55:10 +0200

    Exercises

---
+++ C# OOP Advanced/02 Generics/Exercises/10Tuple/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _10Tuple
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] inputOne = Console.ReadLine().Split();
+            Tuple<string, string> firstTupple = new Tuple<string, string>((inputOne[0] + " " + inputOne[1]), inputOne[2]);
+
+            string[] inputTwo = Console.ReadLine().Split();
+            Tuple<string, int> secondTupple = new Tuple<string, int>((inputTwo[0]), int.Parse(inputTwo[1]));
+
+            string[] inputThree = Console.ReadLine().Split();
+            Tuple<int, double> thirdTupple = new Tuple<int, double>(int.Parse(inputThree[0]), double.Parse(inputThree[1]));
+
+            Console.WriteLine(firstTupple.ToString());
+            Console.WriteLine(secondTupple.ToString());
+            Console.WriteLine(thirdTupple.ToString());
+        }
+    }
+}
---
+++ C# OOP Advanced/02 Generics/Exercises/10Tuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10Tuple
+{
+    public class Tuple<T, U>
+    {
+        public Tuple(T itemOne, U ItemTwo)
+        {
+            this.ItemOne = itemOne;
+            this.ItemTwo = ItemTwo;
+        }
+
+        public T ItemOne { get; protected set; }
+        public U ItemTwo { get; protected set; }
+
+        public override string ToString()
+        {
+            return $"{this.ItemOne} -> {this.ItemTwo}";
+        }
+    }
+}
