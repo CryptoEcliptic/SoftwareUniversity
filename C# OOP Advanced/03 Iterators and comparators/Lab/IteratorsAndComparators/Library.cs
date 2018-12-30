b15f98959b19b6688ba4ada917509e5afa2f1677
CryptoEcliptic <cryptoecliptic@gmail.com>
2018-12-30 17:52:45 +0200

    Lab

---
+++ C# OOP Advanced/03 Iterators and comparators/Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private SortedSet<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books, new BookComparator());
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+            //for (int i = 0; i < books.Count; i++) //Not applicable for sortedSet. Applicable for a list.
+            //{
+            //    yield return this.books[i];
+            //}
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+    }
+}
