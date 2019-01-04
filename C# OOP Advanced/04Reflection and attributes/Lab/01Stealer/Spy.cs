dc459d01560e6525dc3494a91ace97c2ceaf8502
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-01-04 15:21:27 +0200

    Lab

---
+++ C# OOP Advanced/04Reflection and attributes/Lab/01Stealer/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    public string StealFieldInfo(string investigatedClass, params string[] namesOfUnitsToInvestigate)
+    {
+        Type classType = Type.GetType(investigatedClass);
+        FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static
+            | BindingFlags.NonPublic | BindingFlags.Public);
+
+        StringBuilder builder = new StringBuilder();
+
+        Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+        builder.AppendLine($"Class under investigation: {investigatedClass}");
+
+        foreach (FieldInfo field in classFields.Where(f => namesOfUnitsToInvestigate.Contains(f.Name)))
+        {
+            builder.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+        }
+
+        return builder.ToString().Trim();
+
+    }
+
+}
+
---
+++ C# OOP Advanced/04Reflection and attributes/Lab/02HighQualityMistakes/Spy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+
+public class Spy
+{
+    public string AnalyzeAcessModifiers(string className)
+    {
+        Type classType = Type.GetType(className);
+
+        FieldInfo[] fields = classType.GetFields(BindingFlags.Public |
+            BindingFlags.Instance | BindingFlags.Static);
+
+        MethodInfo[] publicMethods = classType.GetMethods(BindingFlags.Public |
+            BindingFlags.Instance | BindingFlags.Static);
+
+        MethodInfo[] nonPublicMethods = classType.GetMethods(BindingFlags.NonPublic |
+           BindingFlags.Instance | BindingFlags.Static);
+
+        StringBuilder sb = new StringBuilder();
+
+        foreach (var item in fields)
+        {
+            sb.AppendLine($"{item.Name} must be private!");
+        }
+
+        foreach (var item in nonPublicMethods.Where(x => x.Name.StartsWith("get")))
+        {
+            sb.AppendLine($"{item.Name} have to be public!");
+        }
+
+        foreach (var item in publicMethods.Where(x => x.Name.StartsWith("set")))
+        {
+            sb.AppendLine($"{item.Name} have to be private!");
+        }
+
+       
+        return sb.ToString().TrimEnd();
+    }
+}
+
