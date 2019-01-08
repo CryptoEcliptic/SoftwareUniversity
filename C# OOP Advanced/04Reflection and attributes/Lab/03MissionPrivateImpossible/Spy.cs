80f4bb1c00d5093bb9c353165340baa0e3b2c83b
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-01-08 18:18:43 +0200

    Lab

---
+++ C# OOP Advanced/04Reflection and attributes/Lab/03MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    public string RevealPrivateMethods(string className)
+    {
+        Type type = Type.GetType(className);
+
+        
+        MethodInfo[] privateMethods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+
+
+        StringBuilder output = new StringBuilder();
+        output.AppendLine($"All Private Methods of Class: {className}");
+        output.AppendLine($"Base Class: {type.BaseType.Name}");
+
+        foreach (var method in privateMethods)
+        {
+            output.AppendLine(method.Name);
+        }
+
+        return output.ToString().TrimEnd();
+    }
+}
---
+++ C# OOP Advanced/04Reflection and attributes/Lab/04Collector/Spy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+
+public class Spy
+{
+    public string CollectGettersAndSetters(string className)
+    {
+        Type typeClass = Type.GetType(className);
+
+        MethodInfo[] methods = typeClass.GetMethods(BindingFlags.NonPublic | BindingFlags.Public
+            | BindingFlags.Instance | BindingFlags.Static);
+
+        StringBuilder output = new StringBuilder();
+
+        foreach (var method in methods.Where(x => x.Name.StartsWith("get")))
+        {
+            output.AppendLine($"{method.Name} will return {method.ReturnType}");
+        }
+
+        foreach (var method in methods.Where(x => x.Name.StartsWith("set")))
+        {
+            output.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+        }
+
+        return output.ToString().TrimEnd();
+    }
+}
+
