5a65fa6ca06ae56e7632ad05298ac5df8d752cfb
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-01-13 18:17:39 +0200

    Exercises

---
+++ C# OOP Advanced/04Reflection and attributes/Exercises/01HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,48 @@
+﻿ namespace P01_HarvestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            Type typeClass = Type.GetType("P01_HarvestingFields.HarvestingFields");
+
+            FieldInfo[] allFields = typeClass.GetFields(BindingFlags.NonPublic | BindingFlags.Public
+                | BindingFlags.Static | BindingFlags.Instance);
+
+            string input = Console.ReadLine();
+
+            while (input != "HARVEST")
+            {
+                if (input == "all")
+                {
+                    foreach (var field in allFields)
+                    {
+                        Console.WriteLine($"{field.Attributes.ToString().ToLower().Replace("family", "protected")} " +
+                        $"{field.FieldType.Name} {field.Name}");
+                    }
+                }
+                else
+                {
+                    FieldInfo[] fieldsToPrint = allFields
+                    .Where(x => x.Attributes
+                    .ToString()
+                    .ToLower()
+                    .Replace("family", "protected") == input)
+                    .ToArray();
+
+                    foreach (var field in fieldsToPrint)
+                    {
+                        Console.WriteLine($"{field.Attributes.ToString().ToLower().Replace("family", "protected")} " +
+                            $"{field.FieldType.Name} {field.Name}");
+                    }
+                }
+                
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
