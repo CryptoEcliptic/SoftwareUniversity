f4b6003d1a0b3ad30abc9765ce5b22288705fc79
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-01-24 17:40:37 +0200

    Lab Unit testing

---
+++ C# OOP Advanced/05 Unit Testing/Lab/Tests/AxeTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skeleton
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void AxeLosesDurabilityAfterAttack()
+        {
+            const int Health = 10;
+            const int Experience = 10;
+            const int Attack = 10;
+            const int Durability = 10;
+
+            Dummy dummy = new Dummy(Health, Experience);
+            Axe axe = new Axe(Attack, Durability);
+
+            axe.Attack(dummy);
+            int expectedDurability = 9;
+
+            Assert.That(expectedDurability.Equals(axe.DurabilityPoints), "Axe Durability doesn't change after attack.");
+        }
+
+        [Test]
+        public void AxeThrowsExceptionIfBboken()
+        {
+            const int Health = 10;
+            const int Experience = 10;
+            const int Attack = 10;
+            const int Durability = 0;
+
+            Dummy dummy = new Dummy(Health, Experience);
+            Axe axe = new Axe(Attack, Durability);
+
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+        }
+    }
+}
---
+++ C# OOP Advanced/05 Unit Testing/Lab/Tests/DummyTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skeleton
+{
+    [TestFixture]
+    public class DummyTests
+    { 
+        [Test]
+        public void DummyLosesHealthIfAttacked()
+        {
+            //Arrange
+            const int Health = 10;
+            const int Experience = 10;
+            const int AttackPoints = 5;
+            Dummy dummy = new Dummy(Health, Experience);
+
+            //Act
+            dummy.TakeAttack(AttackPoints);
+            int expectedHealthAfterAttack = 5;
+
+            //Assert
+            Assert.That(expectedHealthAfterAttack.Equals(dummy.Health));
+        }
+
+        [Test]
+        public void DeadDummyThrowsExceptionIfAttacked()
+        {
+            const int Health = 0;
+            const int Experience = 5;
+            const int AttackPoints = 1;
+
+            Dummy dummy = new Dummy(Health, Experience);
+            
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(AttackPoints));
+        }
+
+        [Test]
+        public void DeadDummyCanGiveExperience()
+        {
+            const int Health = 0;
+            const int Experience = 5;
+
+            Dummy dummy = new Dummy(Health, Experience);
+
+            int experience = dummy.GiveExperience();
+            int expectedReturnValue = 5;
+
+            Assert.That(expectedReturnValue.Equals(experience));
+        }
+
+        [Test]
+        public void AliveDummyCanNotGiveExperience()
+        {
+            const int Health = 2;
+            const int Experience = 5;
+
+            Dummy dummy = new Dummy(Health, Experience);
+
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+        }
+    }
+}
