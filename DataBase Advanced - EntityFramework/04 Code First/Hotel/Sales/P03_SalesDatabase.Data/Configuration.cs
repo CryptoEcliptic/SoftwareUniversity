fcbec6081db507512f595e6dece4eb864f67dd95
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-03-03 14:55:31 +0200

    test addition

---
+++ DataBase Advanced - EntityFramework/04 Code First/Hotel/Sales/P03_SalesDatabase.Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.Data
+{
+    public class Configuration
+    {
+        public const string connectionString = @"Server=NAME-PC\SQLEXPRESS; Database=MinionsDB; Integrated Security=true";
+    }
+}
---
+++ DataBase Advanced - EntityFramework/04 Code First/Hotel/Sales/P03_SalesDatabase.Data/SalesContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Models;
+using System;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {   //TODO AddConfiguration Classes
+        public SalesContext()
+        {
+        }
+
+        public SalesContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                builder.UseSqlServer(Configuration.connectionString);
+            }
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+    }
+}
