30f6a1a39be9922e806fed7a7dd4eef3a9f2c6fd
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-03-09 14:18:36 +0200

    Added ORM Fundamentals exercise

---
+++ DataBase Advanced - EntityFramework/02 ORM Fundamentals/02.ORMFundamentals/MiniORM.App/Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniORM.App.Data
+{
+    public class Configuration
+    {
+        public const string ConnectionString = @"Server=NAME-PC\SQLEXPRESS; Database=MiniORM; Integrated Security=true";
+    }
+}
---
+++ DataBase Advanced - EntityFramework/02 ORM Fundamentals/02.ORMFundamentals/MiniORM.App/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace MiniORM.App
+{
+    using System.Linq;
+    using Data;
+    using Data.Entities;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            SoftUniDbContext db = new SoftUniDbContext(Configuration.ConnectionString);
+
+            db.Employees.Add(new Employee("Rado", "Radov", db.Departments.First().Id, true));
+
+            Employee employee = db.Employees.Last();
+            employee.FirstName = "Rado";
+
+            db.SaveChanges();
+        }
+    }
+}
