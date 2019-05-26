a129d046843fc25d5a2e789a17022f4245c05fe7
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-05-26 12:54:04 +0300

    Refactored StartUp class - added regions

--- C# Web Development Basics/07 WORKSHOP - WEB APPLICATION - DATA VISUALIZATION/IRunesWebApp/IRunesWebApp/Services/UserCookieService.cs
+++ C# Web Development Basics/07 WORKSHOP - WEB APPLICATION - DATA VISUALIZATION/IRunesWebApp/IRunesWebApp/Services/UserCookieService.cs
@@ -2,7 +2,6 @@
 {
     using IRunesWebApp.Services.Contracts;
     using System;
-    using System.Collections.Generic;
     using System.IO;
     using System.Security.Cryptography;
     using System.Text;
